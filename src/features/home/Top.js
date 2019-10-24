60d2d0a691626aa03a3b0775e87bffa7f70252c7
BebopVinh <vinhdo07@gmail.com>
2019-10-24 10:16:22 -0700

    Add Container fluid wrapper

--- src/features/home/Top.js
+++ src/features/home/Top.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import { Parallax } from 'react-scroll-parallax';
-import { Image } from 'semantic-ui-react';
+import React from "react"
+import { Parallax } from "react-scroll-parallax"
+import { Container, Image } from "semantic-ui-react"
 
 const Top = () => (
-    <Parallax className="Top" y={[-20, 20]} tagOuter="figure">
-        <Image src="https://images.unsplash.com/photo-1480365501497-199581be0e66?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60" />
-    </Parallax>
+	<Parallax className="top" y={[-20, 20]}>
+		<Container fluid>
+			<Image
+				src="https://images.unsplash.com/photo-1480365501497-199581be0e66?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+				fluid
+			/>
+		</Container>
+	</Parallax>
 )
 
-export default Top;
+export default Top
