8829b551aefca7c336628aff40c50fb63bc403a3
BebopVinh <vinhdo07@gmail.com>
2019-10-24 10:15:37 -0700

    change classname to lowercase "banner"

--- src/features/home/Banner.js
+++ src/features/home/Banner.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import { Parallax } from 'react-scroll-parallax';
-import { Image } from 'semantic-ui-react';
+import React from "react"
+import { Parallax } from "react-scroll-parallax"
+import { Image } from "semantic-ui-react"
 
 const Banner = () => (
-    <Parallax className="Banner" y={[-50, 50]} tagOuter="figure">
-        <Image src="https://images.unsplash.com/flagged/photo-1567934150921-7632371abb32?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80" />
-    </Parallax>
+	<Parallax y={[-50, 50]}>
+		<Image
+			src="https://images.unsplash.com/flagged/photo-1567934150921-7632371abb32?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80"
+			className="banner"
+		/>
+	</Parallax>
 )
 
-export default Banner;
+export default Banner
