b53979deea8ad31a4c927e2c683e6fa6c9ec8339
github username <joanrigdon@gmail.com>
2019-10-25 12:27:27 -0400

    updated button text and link for each activity

--- src/features/home/ActivitiesData.js
+++ src/features/home/ActivitiesData.js
@@ -3,7 +3,8 @@
     title: "Computer Programming Education",
     subtitle: "One Step at a Time",
     description: "With gender equity always in mind, we strive to find new strategies for dealing with the challenge of advancing global economic development. Computer literacy is something that we take very seriously, and our team is working each and every day to make a positive impact. Contact us to learn more about our commitment to this cause.",
-    buttonLink: "Help us Succeed"
+    buttonText: "Help us Succeed",
+    buttonLink: 'https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=info@codigoecuador.com&item_name=Imagina%20C%C3%B3digo%20Ecuador&currency_code=USD'
   }
 
   const internship = {
@@ -11,7 +12,9 @@
     title: "Internship Placement",
     subtitle: "Experiential Learning",
     description: "At Código Ecuador, we are dedicated to connecting Ecuadorian high school students to rewarding hands-on experiences. Through cooperation and global community empowerment, we believe we can facilitate access to valuable learning opportunities. We are always striving to make a difference, and invite you to learn more and lend your support.",
-    buttonLink: "Become a Partner"
+    buttonText: "Become a Partner",
+    buttonLink: "mailto:info@codigoecuador.com"
+
   }
 
 const entrepreneurship = {
@@ -19,7 +22,8 @@ const entrepreneurship = {
   title: "Encourage Entrepreneurship",
   subtitle: "One Step at a Time",
   description: "With our organization’s mission always in mind, we strive to find new strategies to reach our goal. To preserve Ecuador's beautiful culture of entrepreneurship, Código Ecuador works to tear down walls, pave roads to resources, and encourage women to build opportunity. Contact us to learn more about how we make this happen.",
-  buttonLink: "Get in Touch"
+  buttonText: "Get in Touch",
+  buttonLink:"/contact"
 }
 
 export const activities = [programming, internship, entrepreneurship]
