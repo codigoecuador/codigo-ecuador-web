b41c3b069692cd8df60954459a68e97de2c551bc
github username <joanrigdon@gmail.com>
2019-10-25 11:43:16 -0400

    added activity component with objects data

--- src/features/home/AboutBlurb.js
+++ src/features/home/AboutBlurb.js
@@ -1,32 +1,29 @@
 import React from 'react'
-import { Grid } from 'semantic-ui-react'
+import { Grid, Container, Image } from 'semantic-ui-react'
 
 const AboutBlurb = () => {
     return (
         <div>
-          <Grid>
-            <Grid.Row>
-              <Grid.Column width={8}>
-              <h1>First Column</h1>
-              <h1>A Distinct Identity</h1>
+          <Container>
+            <Grid>
+              <Grid.Row>
+                <Grid.Column width={8}>
+                <h1>First Column</h1>
+                <Image src="https://static.wixstatic.com/media/6ec8ab5092ba4daa8db8d3106847341d.jpg/v1/crop/x_1049,y_0,w_2909,h_3414/fill/w_920,h_1080,al_c,q_85,usm_0.66_1.00_0.01/Smiling%20Student.webp" />
 
-              <p>
-              Código Ecuador is a nonprofit organization working to promote technology skills and computer programming careers for high school students in Ecuador, with a focus on female empowerment. We believe that improving access to computer science curricula can promote global equity, and that collective action can greatly impact the world.
-              </p>
-              </Grid.Column>
+                </Grid.Column>
 
-              <Grid.Column width={8}>
-              <h1>Second Column</h1>
-              <h1>A Distinct Identity</h1>
+                <Grid.Column width={8}>
+                <h1>Second Column</h1>
+                <h1>A Distinct Identity</h1>
 
-              <p>
-              Código Ecuador is a nonprofit organization working to promote technology skills and computer programming careers for high school students in Ecuador, with a focus on female empowerment. We believe that improving access to computer science curricula can promote global equity, and that collective action can greatly impact the world.
-              </p>
-              </Grid.Column>
-            </Grid.Row>
-
-
-          </Grid>
+                <p>
+                Código Ecuador is a nonprofit organization working to promote technology skills and computer programming careers for high school students in Ecuador, with a focus on female empowerment. We believe that improving access to computer science curricula can promote global equity, and that collective action can greatly impact the world.
+                </p>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Container>
 
         </div>
     )
---
+++ src/features/home/Activities.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const Activities = () => {
+
+render() {
+  const programming = {
+    title: "Computer Programming Education",
+    subtitle: "One Step at a Time",
+    description: "With gender equity always in mind, we strive to find new strategies for dealing with the challenge of advancing global economic development. Computer literacy is something that we take very seriously, and our team is working each and every day to make a positive impact. Contact us to learn more about our commitment to this cause.",
+    buttonLink: "Help us Succeed"
+  }
+
+  const internship = {
+    title: "Internship Placement",
+    subtitle: "Experiential Learning",
+    description: "At Código Ecuador, we are dedicated to connecting Ecuadorian high school students to rewarding hands-on experiences. Through cooperation and global community empowerment, we believe we can facilitate access to valuable learning opportunities. We are always striving to make a difference, and invite you to learn more and lend your support.",
+    buttonLink: "Become a Partner"
+  }
+
+const entrepreneurship = {
+  title: "Encourage Entrepreneurship",
+  subtitle: "One Step at a Time",
+  description: "With our organization’s mission always in mind, we strive to find new strategies to reach our goal. To preserve Ecuador's beautiful culture of entrepreneurship, Código Ecuador works to tear down walls, pave roads to resources, and encourage women to build opportunity. Contact us to learn more about how we make this happen.",
+  buttonLink: "Get in Touch"
+}
+
+let projects = [programming, internship, entrepreneurship]
+
+}
+    return (
+        <div>
+
+
+        </div>
+    )
+}
+
+export default Activities
+
+
+// //<Card.Group itemsPerRow={2}>
+//             {projects.map(project =>
+//               <ProjectCard
+//                 key={project.id} project={project} padding="20px"/>
+//               )}
+//           </Card.Group>
