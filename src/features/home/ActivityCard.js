1ff7acf64776a6aee9b258842b7ae558fcb70fcd
github username <joanrigdon@gmail.com>
2019-10-25 13:10:00 -0400

    rendering cards, added images to cards

--- src/features/home/ActivityCard.js
+++ src/features/home/ActivityCard.js
@@ -1,15 +1,24 @@
 import React from 'react'
-import { Card } from 'semantic-ui-react'
+import { Card, Button, Image} from 'semantic-ui-react'
 
 
-const ActivityCard = ({id, title, subtitle, description, buttonLink}) => {
+
+const ActivityCard = ({id, title, subtitle, description, buttonText, buttonLink,image}) => {
 
     return (
-        <Card
-           header={title}
-           meta={subtitle}
-           description={description}
-         />
+        <Card >
+            <Image src={image}/>
+            <Card.Content >
+                <Card.Header>{title}</Card.Header>
+                <h3><i>{subtitle}</i></h3>
+                <Card.Description>{description}</Card.Description>
+
+
+            </Card.Content>
+            <Card.Content className='center' extra >
+                <Button ><a href={buttonLink}>{buttonText}</a></Button>
+            </Card.Content>
+        </Card>
     )
 }
 
