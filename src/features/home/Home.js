095c38b94cad7346a14ac5c99370721c66fa122e
github username <joanrigdon@gmail.com>
2019-10-31 22:29:25 -0400

    made cover image responsive

--- src/features/home/Home.js
+++ src/features/home/Home.js
@@ -1,20 +1,29 @@
 import React from 'react'
 import AboutBlurb from './AboutBlurb'
+import { logo } from './codigo-logo-small.jpg'
 
 
 const Home = () => {
     return (
-        <div class="image">
+        <>
 
-            <img src="https://images.unsplash.com/photo-1498661694102-0a3793edbe74?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1893&q=80" alt="young Latin American women"/>
+            <img className="image" src="https://images.unsplash.com/photo-1498661694102-0a3793edbe74?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1893&q=80" alt="young Latin American women"/>
             <div className="overlay center">
                 <span className="massive">Código Ecuador</span>
                 <h1>help us empower the women of Ecuador by teaching them to code</h1>
             </div>
-            <AboutBlurb className="about" />
+
+            <div className="about">
+              <AboutBlurb />
+              <br/>
+            </div>
+
+            <div className="logo">
+              {logo}>
+            </div>
 
 
-        </div>
+        </>
     )
 }
 
