fec9253d59214e4b080101683ef7052e0d6dfad8
Lizzie <lchan217@gmail.com>
2019-11-01 16:03:47 -0700

    add subscribe form

--- src/features/donate/Donate.js
+++ src/features/donate/Donate.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Card, Button } from 'semantic-ui-react'
+import { Container, Card, Button, Form} from 'semantic-ui-react'
 import {donateData} from './DonateData'
 import DonateCard from './DonateCard'
 
@@ -37,14 +37,22 @@ const Donate = () => {
                 <b>Email:</b> info@codigoecuador.com<a href="mailto:info@codigoecuador.com"> <i class="envelope outline icon"></i></a><br/>
                 <b>Twitter:</b> @CodigoEC<a href="https://twitter.com/CodigoEC" target="_blank"> <i class="twitter icon"></i></a><br/>
                 <br/><Button ><a href='https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=info@codigoecuador.com&item_name=Imagina%20C%C3%B3digo%20Ecuador&currency_code=USD' target="_blank">Donate Now</a></Button>
+                <br/><div className="center">Código Ecuador is a 501(C)(3) organization under the International Intellectual Exchange Initiative Inc. </div>
+              </div>
+              <br/>
+              <div className="center">
+                need to add functionality
+                <h2>Subscribe Now</h2>
+                  <Form>
+                    <Form.Field >
+                      <input placeholder='Email' style={{width: 400}} />
+                    </Form.Field>
+                  </Form>
+                  <br/>
+                  <button className="ui submit button" type="submit">Submit</button>
               </div>
             <br/>
-            <br/>
-            <div className="center">Código Ecuador is a 501(C)(3) organization under the International Intellectual Exchange Initiative Inc. </div>
 
-            subscribe form
-           donate button
-           copyright
            </Container>
         </div>
     )
