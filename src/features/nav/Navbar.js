1ec5d30da54aae2c8f582a677a3d03443dffe6e5
Lizzie <lchan217@gmail.com>
2019-11-01 09:20:52 -0700

    fixed routes

--- src/features/nav/Navbar.js
+++ src/features/nav/Navbar.js
@@ -1,32 +1,25 @@
 import React from "react";
-import { Link, Switch, Route } from 'react-router-dom'
+import {Link} from 'react-router-dom';
 import About from '../about/About'
 import Blog from '../blog/Blog'
 import Donate from '../donate/Donate'
 import Home from '../home/Home'
 import Contact from '../contact/Contact'
+import Activities from '../activities/Activities'
 
 const Navbar = () => {
+
           return (
+
+
                  <div className='main-navbar navbar'>
                    <Link className="active" to='/'>Home</Link>
+                  <Link exact className="active" to="/about">About</Link>
+                  <Link exact className="active" to="/programs">Programs</Link>
+                  <Link exact className="active" to="/blog">Blog</Link>
+                  <Link exact className="active" to="/donate">Donate</Link>
+                </div>
 
-                  <Link exact className="active" to="/about">
-                    About
-                  </Link>
-
-                  <Link exact className="active" to="/programs">
-                    Programs
-                  </Link>
-
-                  <Link exact className="active" to="/blog">
-                    Blog
-                  </Link>
-
-                  <Link exact className="active" to="/donate">
-                    Donate
-                  </Link>
-              </div>
           );
         };
 
