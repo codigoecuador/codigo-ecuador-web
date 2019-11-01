f00a9c4eac87d4ae112d6742e6c2b2e9139d8e24
Lizzie <lchan217@gmail.com>
2019-11-01 15:32:47 -0700

    add contact info to bottom

--- src/features/donate/Donate.js
+++ src/features/donate/Donate.js
@@ -20,9 +20,10 @@ const Donate = () => {
           <br/>
           <Container>
             <h1 className='center'><span style={gold}>Código</span><span style={blue}> Ecuador</span></h1>
-           <h3 className='center'>You can promote technology careers for women in Ecuador by
-             helping us with supplies, equipment, and space from which to work.</h3>
+           <h2 className='center'>You can promote technology careers for women in Ecuador by
+             helping us with supplies, equipment, and space from which to work.</h2>
            <br/>
+                 <div>need to put an action like hover over this</div>
            <Card.Group itemsPerRow={3}>
              {donateData.map(donate =>
                  <DonateCard
@@ -30,8 +31,13 @@ const Donate = () => {
                  )}
              </Card.Group>
 
+            <h3>Contact us if you can help!</h3>
+            <b>Phone:</b> +1 571-216-5441 <br/>
+            <b>Email:</b> info@codigoecuador.com<a href="mailto:info@codigoecuador.com"> <i class="envelope outline icon"></i></a><br/>
+            <b>Twitter:</b> @CodigoEC<a href="https://twitter.com/CodigoEC" target="_blank"> <i class="twitter icon"></i></a><br/>
+
            subscribe form
-           phone contact & email?
+           donate button
            501c3 org statement
            copyright
            </Container>
