fce2dcc88cbe584a7914741ca689a36380e5ebe1
Lizzie <lchan217@gmail.com>
2019-11-01 12:29:40 -0700

    add title and paragraph

--- src/features/donate/Donate.js
+++ src/features/donate/Donate.js
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Container } from 'semantic-ui-react'
 
 const Donate = () => {
+  var gold = {
+	color: 'rgb(186, 156, 91)'
+};
+  var blue = {
+	color: 'rgb(18, 43, 79)'
+};
     return (
         <div>
-           <h1>Donate!</h1>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <Container>
+            <h1><span style={gold}>Codigo</span><span style={blue}> Ecuador</span></h1>
+           <h3>You can promote technology careers for women in Ecuador by
+             helping us with supplies, equipment, and space from which to work.</h3>
+           </Container>
         </div>
     )
 }
