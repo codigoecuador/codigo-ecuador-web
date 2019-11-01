9ef665157e01a5f5f5a94f094eccf217506c10ca
github username <joanrigdon@gmail.com>
2019-11-01 10:27:56 -0400

    super basic styling

--- src/features/activities/Activities.js
+++ src/features/activities/Activities.js
@@ -2,10 +2,22 @@ import React from 'react'
 import { Card, Container } from 'semantic-ui-react'
 import {activities} from './ActivitiesData'
 import ActivityCard from './ActivityCard'
+import NavBar from '../nav/Navbar'
 
 const Activities = () => {
     return (
         <div>
+          <NavBar />
+          <br/>
+          <br/>
+          <br/>
+          <br/>
+          <div className="big center">
+            <br/>
+            Our work makes a difference.
+          </div>
+          <br/>
+          <br/>
           <Container>
               <Card.Group itemsPerRow={2}>
                   {activities.map(activity =>
@@ -14,6 +26,8 @@ const Activities = () => {
                       )}
               </Card.Group>
           </Container>
+          <br/>
+          <br/>
         </div>
     )
 }
