bbb1670072c1f5ae8a2132000475f142f5e63919
Lizzie <lchan217@gmail.com>
2019-11-01 09:06:33 -0700

    need to fix routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./App.css";
-import { Route, Switch } from "react-router-dom";
 import Navbar from "./features/nav/Navbar";
 import Home from "./features/home/Home";
 import About from "./features/about/About";
@@ -8,11 +7,21 @@ import Blog from "./features/blog/Blog";
 import Donate from "./features/donate/Donate";
 import Contact from "./features/contact/Contact";
 import Activities from './features/activities/Activities'
+import {BrowserRouter as Router,Route, Switch,Link} from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Home />
+      <Router>
+        <div>
+          <Route exact path='/'component={Home}></Route>
+          <Route exact path='/about' component={About}></Route>
+          <Route exact path='/activities' component={Activities}></Route>
+          <Route exact path='/blog' component={Blog}></Route>
+          <Route exact path='/donate' component={Donate}></Route>
+        </div>
+      </Router>
+      <Navbar />
     </div>
   );
 }
--- src/features/nav/Navbar.js
+++ src/features/nav/Navbar.js
@@ -1,21 +1,16 @@
 import React from "react";
-import {BrowserRouter as Router,Route, Switch,Link} from 'react-router-dom';
+import { Link, Switch, Route } from 'react-router-dom'
 import About from '../about/About'
 import Blog from '../blog/Blog'
 import Donate from '../donate/Donate'
 import Home from '../home/Home'
 import Contact from '../contact/Contact'
 
-const NavBar = () => {
-
-
-
+const Navbar = () => {
           return (
-            <Router>
-           
-                 <div className='main-navbar'>
+                 <div className='main-navbar navbar'>
                    <Link className="active" to='/'>Home</Link>
-              
+
                   <Link exact className="active" to="/about">
                     About
                   </Link>
@@ -31,31 +26,8 @@ const NavBar = () => {
                   <Link exact className="active" to="/donate">
                     Donate
                   </Link>
-
-                  <Switch>
-                    <Route exact path='/'r ender={()=>(<Home />)}></Route>
-                    <Route path='/about' render={()=>(<About />)}></Route>
-                    <Route path='/prgorams'></Route>
-                    <Route path='/blog' render={()=>(<Blog />)}></Route>
-                    <Route path='/donate' render={()=>(<Donate />)}></Route>
-                  </Switch>
-                 
-              
-
-            
-      
               </div>
-             
-
-              </Router>
-
-                  
-
-                
-           
-            
           );
         };
 
-export default NavBar;
-
+export default Navbar;
