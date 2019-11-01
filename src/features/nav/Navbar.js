8ec7911125a7ae7f17d318a1106dcec70a465dcc
Lizzie <lchan217@gmail.com>
2019-11-01 09:37:04 -0700

    delete main-nav class

--- src/features/nav/Navbar.js
+++ src/features/nav/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
           return (
 
 
-                 <div className='main-navbar navbar'>
+                 <div className='navbar'>
                    <Link className="active" to='/'>Home</Link>
                   <Link exact className="active" to="/about">About</Link>
                   <Link exact className="active" to="/activities">Activities</Link>
