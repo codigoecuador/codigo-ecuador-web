f086d3219fc1a65f80ea1588f2cc786c50094c40
Lizzie <lchan217@gmail.com>
2019-11-01 17:30:08 -0700

    add extra break

--- src/features/donate/Donate.js
+++ src/features/donate/Donate.js
@@ -37,7 +37,7 @@ const Donate = () => {
                 <b>Email:</b> info@codigoecuador.com<a href="mailto:info@codigoecuador.com"> <i class="envelope outline icon"></i></a><br/>
                 <b>Twitter:</b> @CodigoEC<a href="https://twitter.com/CodigoEC" target="_blank"> <i class="twitter icon"></i></a><br/>
                 <br/><Button ><a href='https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=info@codigoecuador.com&item_name=Imagina%20C%C3%B3digo%20Ecuador&currency_code=USD' target="_blank">Donate Now</a></Button>
-                <br/><div className="center">Código Ecuador is a 501(C)(3) organization under the International Intellectual Exchange Initiative Inc. </div>
+                <br/><br/><div className="center">Código Ecuador is a 501(C)(3) organization under the International Intellectual Exchange Initiative Inc. </div>
               </div>
               <br/>
               <div className="center">
