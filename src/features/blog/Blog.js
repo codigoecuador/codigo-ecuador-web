8800734582628856f8db86cffcd5e90773f97650
Burak Kocak <burakkocak884@hotmail.com>
2019-11-01 22:48:00 -0400

    blog cards

--- src/features/blog/Blog.js
+++ src/features/blog/Blog.js
@@ -1,6 +1,11 @@
 import React, {Component} from 'react'
-import ReactHtmlParser from 'react-html-parser'
-import {Link} from 'react-router-dom'
+//react carousel imported
+import Carousel, { Dots } from '@brainhubeu/react-carousel';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import {Link} from 'react-router-dom';
+
 
 class Blog  extends Component {
     constructor(props){
@@ -9,57 +14,51 @@ class Blog  extends Component {
             blogs: []
         }
     }
+    
 
             componentDidMount(){
-                        return fetch(`https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@codigoecuador`)
+                        return fetch(`https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@burakkocak884`)
                         .then(response => response.json())
                         .then(blogs =>   this.setState({blogs: blogs})
                             );
                 }
-                
+      //react carousel installed with < npm i @brainhubeu/react-carousel > in the terminal          
 
 
     render(){
              console.log("are my blogs in?",this.state)
-         
+           
            
              if(this.state.blogs.items){
                 const {items} = this.state.blogs
-                return(
-                    <div className="background-img">
-                             
+                var settings = {
+                  dots: true,
+                  infinite: true,
+                  speed: 2000,
+                  slidesToShow: 2,
+                  slidesToScroll: 2,
+                  autoplay: true,
+                  autoplayspeed: 4000
+                };
+                return (
+                  <div className='blog-container'>
+                  <Slider {...settings}>
+                    {items.map((blog,index)=>(
+                    <div className='card'>
+                      <a href={blog.link}><img src={blog.thumbnail} className='blog-card-image'/></a>
+                      <p className='blog-title'>{blog.title}</p>
+                      <p><span className='blog-author'>{blog.author}</span>  Published on: <span className='blog-date'>{blog.pubDate}</span></p>
+                    </div>
+                     
+                   
                     
-                  
-                    <div id='card-container' class="ui link cards">
-                             
-                         {items.map((blog,index)=>(
-                    <div class="card">
-                      <div class="image">
-                        <img src={`${blog.thumbnail}`}/>
-                      </div>
-                      <div class="content">
-                        <div class="header"><a href={`${blog.link}`} target="_blank" rel="noopener noreferrer">{blog.title}</a></div>
-                        <div class="meta">
-                          <a></a>
-                        </div>
-                        <div class="description">
-                          #{blog.categories.join('# ')}}
-                        </div>
-                      </div>
-                      <div class="extra content">
-                        <span class="right floated">
-                          {blog.pubDate}
-                        </span>
-                        <span>
-                          <i class="user icon"></i>
-                          {blog.author}
-                        </span>
-                      </div>
-                    </div>
-                         ))}
-                    </div>
-                    </div>
-                        )}else{
+                    ))}
+                  </Slider>
+                  </div>
+                );
+              }
+                   
+                       else{
                  return (
                 <div>No blogs</div>
             )
