5620122f8081c3b3e5e230971ae51de24e950cf1
github username <joanrigdon@gmail.com>
2019-11-03 15:59:27 -0500

    added buttons div and placed it on resources page

--- src/features/resources/Resources.js
+++ src/features/resources/Resources.js
@@ -1,14 +1,26 @@
 import React from 'react'
+import { Button, Container } from 'semantic-ui-react'
 
 
 const Resources = () => {
     return (
-        <div>
-
-            <h1>Resources!</h1>
-
-
-        </div>
+      <div className="resources">
+        <h1>Resources!</h1>
+        <br/>
+        <br/>
+        <Button.Group vertical>
+          <Button>For Entrepreneurs</Button>
+          <br/>
+          <br/>
+          <Button>For Students</Button>
+          <br/>
+          <br/>
+          <Button>For Parents</Button>
+          <br/>
+          <br/>
+          <Button>For Educators</Button>
+        </Button.Group>
+      </div>
     )
 }
 
