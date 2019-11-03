477ac296f2a1b21af74d31e4e494112dc4cd29cc
github username <joanrigdon@gmail.com>
2019-11-03 16:10:27 -0500

    conditionally rendering div, need to place it in right spot on page

--- src/features/resources/Resources.js
+++ src/features/resources/Resources.js
@@ -1,27 +1,57 @@
-import React from 'react'
-import { Button, Container } from 'semantic-ui-react'
+import React, { Component } from 'react'
+import { Button } from 'semantic-ui-react'
+import Courses from './Courses'
 
 
-const Resources = () => {
+class Resources extends Component {
+    state = {
+      showing: ""
+    }
+
+    handleClick = (event) => {
+      this.setState({ showing: event.target.value})
+    }
+
+
+
+  render(){
+
+    let content
+
+    if(this.state.showing === "students"){
+      content = <Courses />
+    }
+
+
     return (
-      <div className="resources">
-        <h1>Resources!</h1>
-        <br/>
-        <br/>
-        <Button.Group vertical>
-          <Button>For Entrepreneurs</Button>
+      <>
+        <div className="resources">
+          <h1>Resources!</h1>
           <br/>
           <br/>
-          <Button>For Students</Button>
-          <br/>
-          <br/>
-          <Button>For Parents</Button>
-          <br/>
-          <br/>
-          <Button>For Educators</Button>
-        </Button.Group>
-      </div>
+          <Button.Group vertical>
+            <Button onClick={this.handleClick} value="entrepreneurs">For Entrepreneurs</Button>
+            <br/>
+            <br/>
+            <Button onClick={this.handleClick} value="students">For Students</Button>
+            <br/>
+            <br/>
+            <Button onClick={this.handleClick} value="parents">For Parents</Button>
+            <br/>
+            <br/>
+            <Button onClick={this.handleClick} value="educators">For Educators</Button>
+          </Button.Group>
+        </div>
+
+        <div className="showing">
+          { content }
+        </div>
+      </>
     )
+
+  }
+
+
 }
 
 export default Resources
