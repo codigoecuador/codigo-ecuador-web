42f4a1430aca8058969ec1a70b4cb635a22e7712
github username <joanrigdon@gmail.com>
2019-11-04 11:38:47 -0500

    minor changes to donate styling and css

--- src/features/contact/Contact.js
+++ src/features/contact/Contact.js
@@ -3,7 +3,15 @@ import React from 'react'
 const Contact = () => {
     return (
         <div>
-            <h1>Contact!</h1>
+        <h2 className="center">Contact us if you can help!</h2>
+          <div className="contact-info center">
+            <b>Phone:</b> +1 571-216-5441 <br/>
+            <b>Email:</b> info@codigoecuador.com<a href="mailto:info@codigoecuador.com"> <i class="envelope outline icon"></i></a><br/>
+            <b>Twitter:</b> @CodigoEC<a href="https://twitter.com/CodigoEC" target="_blank"> <i class="twitter icon"></i></a><br/>
+            <br/><Button ><a href='https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=info@codigoecuador.com&item_name=Imagina%20C%C3%B3digo%20Ecuador&currency_code=USD' target="_blank">Donate Now</a></Button>
+            <br/><br/><div className="center">Código Ecuador is a 501(C)(3) organization under the International Intellectual Exchange Initiative Inc. </div>
+          </div>
+          <br/>
         </div>
     )
 }
--- src/features/donate/DonateCard.js
+++ src/features/donate/DonateCard.js
@@ -6,16 +6,15 @@ import {Image, Card, Reveal} from 'semantic-ui-react'
 const DonateCard = ({topic, summary, image}) => {
 
     return (
-      <div className='donate-cards'>
-        <div class="ui grid">
-          <div class="three wide column"></div>
-          <div class="three wide column"></div>
-          <div class="three wide column"></div>
-          <div class="ui medium fade reveal image">
-            <img class="visible content" src={image}/>
-            <div class="hidden content"><h2>{topic}</h2> {summary}</div>
-          </div>
-        </div>
+      <div className='donateCardsGroup'>
+
+          <Card>
+            <div class="ui massive fade reveal image">
+              <img class="visible content donateCard" src={image}/>
+              <div class="hidden content"><h2>{topic}</h2> {summary}</div>
+            </div>
+          </Card>
+
         <br/><br/><br/><br/><br/><br/><br/><br/>
       </div>
     )
