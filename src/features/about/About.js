8cf9a4136e621da0812a4479866646537d1ebfb0
github username <joanrigdon@gmail.com>
2019-11-03 21:45:09 -0500

    changed file structure - team and partners nestd under about

--- src/features/about/About.js
+++ src/features/about/About.js
@@ -1,19 +1,17 @@
 import React from 'react'
-import { Parallax } from 'react-scroll-parallax';
-import { Image, Header } from 'semantic-ui-react'
+
+
 
 const About = () => {
+
     return (
-        <div>
+      <div className="massive">
+        <h1>About!</h1>
 
-            <h1>About!</h1>
-              <Parallax className="custom-class" y={[-50, 50]} tagOuter="figure">
-                <Image className='aboutImage' size='huge' fluid src='https://images.unsplash.com/photo-1572219479068-8a05d5310ba1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80' />
-              </Parallax>
-                <Image className='topAboutImage' size='huge' src='https://images.unsplash.com/photo-1569025743873-ea3a9ade89f9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80' />
-
-        </div>
+      </div>
     )
-}
+  }
+
+
 
 export default About
