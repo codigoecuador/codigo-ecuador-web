77d7c5da5ef835b4e627b506c52ab1176803db5c
bebopvinh <vinhdo07@gmail.com>
2019-11-03 18:16:29 -0800

    add JSON data and images resources for Partners page

---
+++ src/features/partners/partners_data.js
@@ -0,0 +1,29 @@
+import kodluyoruz from "./kodluyoruz.webp"
+import greatAdventureLab from "./greatAdventureLab.webp"
+const kodluyoruzData = {
+	name: "Kodluyoruz",
+	image: kodluyoruz,
+	content: [
+		"Kodluyoruz.org is a software movement that promotes code literacy and adds value to the future of young people in the software field in order to improve every segment of society by using technology."
+	]
+}
+const greatAdventureLabData = {
+	name: "The Great Adventure Lab",
+	image: greatAdventureLab,
+	content: [
+		"The Great Adventure Lab is a fun, educational after school program that serves schools in the DC-area with an on-going mission to make STEM fun for the next generation."
+	]
+}
+
+const juventud = {
+	name: "Juventud del Mañana",
+	image:
+		"https://media.istockphoto.com/photos/girlfriends-in-the-city-picture-id700702502",
+	content: [
+		"Juventud del Mañana is an association of dedicated, community - minded people who saw and responded to the needs of our youth.",
+		"It is a teen drop-in centre that provides a safe, drug, and alcohol - free, place designed exclusively to meet their needs.It is a place to enjoy their activities, whether it be playing basketball, practicing with their band, playing games, listening to music, and holding dances.",
+		"Beginning in October, Juventud del Mañana will provide educational programs and on the job experiences designed to help teens reach their full potential."
+	]
+}
+
+export const data = [kodluyoruzData, greatAdventureLabData, juventud]
