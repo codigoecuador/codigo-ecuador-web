764baed9286bbea9d57c92f71b4e676bb7a79efd
github username <joanrigdon@gmail.com>
2019-11-05 14:09:43 -0500

    created header

--- src/App.js
+++ src/App.js
@@ -1,40 +1,13 @@
 import React from "react"
-import "./App.css"
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-import Navbar from "./features/header/Navbar"
-import Home from "./features/home/Home"
-import About from "./features/about/About"
-import Partners from "./features/about/partners/Partners"
-import Team from "./features/about/team/Team"
-import Blog from "./features/blog/Blog"
-import Donate from "./features/donate/Donate"
-import ResourcesContainer from "./features/resources/ResourcesContainer"
-import Activities from "./features/activities/Activities"
-import Logo from "./features/header/Logo"
+import "./App.css" 
+import Header from './features/header/Header'
+
 
 
 function App() {
 
 	return (
-		<Router>
-			<div>
-				<Navbar />
-				<Switch>
-					<Route exact path="/" render={() => <Home />}></Route>
-					<Route exact path="/about" render={() => <About />}></Route>
-					<Route exact path="/activities" render={() => <Activities />}></Route>
-					<Route exact path="/about/partners" render={() => <Partners />} ></Route>
-          <Route exact path="/about/team" render={() => <Team />} ></Route>
-					<Route exact path="/resources" render={() => <ResourcesContainer />}></Route>
-					<Route exact path="/blog" render={() => <Blog />}></Route>
-					<Route exact path="/donate" render={() => <Donate />}></Route>
-				</Switch>
-			</div>
-			<div className="banner">
-				<Logo className="logo" />
-			</div>
-      <br/>
-		</Router>
+		<Header />
 	)
 }
 
