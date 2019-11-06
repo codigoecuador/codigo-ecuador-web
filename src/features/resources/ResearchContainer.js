a6fdb2e503b1adbe599fecb398c6bd4c65661d0f
github username <joanrigdon@gmail.com>
2019-11-05 20:17:51 -0500

    added wavemakers and youth employment reports

--- src/features/resources/ResearchContainer.js
+++ src/features/resources/ResearchContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Container, Divider, Image } from 'semantic-ui-react'
+import { Divider, Image } from 'semantic-ui-react'
 import { researchLinks } from './ResearchLinks'
 
 
@@ -10,30 +10,39 @@ class ResearchContainer extends Component {
   render(){
 
     return (
-      <>
-        <div className="researchHeadline">
-          <span className="gold headline">Research</span>
+
+      <div className="researchContainer">
+        <br/>
+
+        <div className="researchBanner">
+          <div className="center">
+            <span className="gold headline">Research</span>
+          </div>
+          <br/>
+          <div className="researchText">
+            <h1 className="center">We model Código Ecuador on research and insights from reputable economic development experts.</h1>
+
+            <p>Coding education is making a huge impact throughout the developing world. People are learning the technical skills necessary to build tomorrow's cutting-edge applications. From Colombia to Lebanon, nonprofit education is opening doors in high paying technology careers for thousands of women and individuals. The work our organizations do are sparking a new wave of research in how we can create a more equal world.</p>
+          </div>
         </div>
 
+        <br/>
 
-        <Container className="researchContainer">
-          <br/>
-          {researchLinks.map(research =>
-            <>
-              <a href={research.link}><Image src={research.image} className="researchImage"/></a>
-              <div className="researchDescription">{research.description}</div>
-              <Divider />
-              <br/>
-              <br/>
-            </>
-          )}
+        <br/>
+        {researchLinks.map(research =>
+          <>
+            <h1 className="center">{research.title}</h1>
+            <h2 className="center">{research.subtitle}</h2>
+            <a href={research.link}><Image src={research.image} className="researchImage"/></a>
+            <div className="researchText">{research.description}</div>
+            <Divider />
+            <br/>
+            <br/>
+          </>
+        )}
 
-        </Container>
+      </div>
 
-
-
-
-      </>
     )
   }
 }
--- src/features/resources/ResearchLinks.js
+++ src/features/resources/ResearchLinks.js
@@ -1,5 +1,25 @@
-const buildingSkills = {
+const youth = {
   id: 1,
+  title: "Coding Bootcamps for Youth Employment",
+  subtitle: "World Bank Report",
+  description: "A framework of best practice for future projects in technology upskilling in the developing world. This World Bank report highlights the results of a randomized controlled trial (RCT) carried out in Medellín (Colombia), complemented with qualitative studies in Beirut (Lebanon) and Nairobi (Kenya).",
+  link: 'https://openknowledge.worldbank.org/bitstream/handle/10986/29742/125169-WP-P156294-PUBLIC-decoding-bootcamps.pdf?sequence=1&isAllowed=y',
+  image: 'https://www.youthpower.org/sites/default/files/inline-images/coding%20bootcamps.PNG'
+}
+
+const women = {
+  id: 2,
+  title: "Women Wavemakers",
+  subtitle: "World Bank Report",
+  description: "The insights in this World Bank report draw on interviews with 25 coding bootcamps and 7 digital skills programs in 22 countries. The goal is to share insights and strategies from practitioners around the world on ways to recruit and retain women in coding bootcamps.",
+  link: "http://documents.worldbank.org/curated/en/682341531982131260/pdf/AUS23845-REVISED-july-31-Women-Wavemakers-Recruiting-and-Retaining-Women-in-Coding-Bootcamps-July2018-5.pdf",
+  image: "https://pbs.twimg.com/media/DjKsfoyW4AElTk5.jpg"
+
+}
+
+
+const skills = {
+  id: 3,
   title: "Coding Bootcamps: Building Future-Proof Skills through Rapid Skills Training",
   subtitle: "World Bank Report",
   description: "This report is part of the Rapid Technology Skills Technical Assistance (“Decoding Bootcamps”) initiative by the World Bank, which aims to collect and share examples and lessons of bootcamps in emerging markets, and measure the impact of bootcamp training on youth employment in selected countries.",
@@ -8,7 +28,7 @@ const buildingSkills = {
 }
 
 const guide = {
-  id: 2,
+  id: 4,
   title: "Coding Bootcamps: Guide for Practitioners",
   subtitle: "World Bank Report",
   description: "This toolkit provides a step by step guide for policymakers and practitioners to develop coding bootcamp programs. The toolkit provides advice on what it takes to organize training from “scratch,” and hence help form an opinion on the ultimate model on which to settle.",
@@ -19,4 +39,4 @@ const guide = {
 
 
 
-export const researchLinks = [buildingSkills, guide ]
+export const researchLinks = [youth, women, skills, guide ]
