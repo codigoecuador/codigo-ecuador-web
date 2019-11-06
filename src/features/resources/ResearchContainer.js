e5ea069fd27d25fab1b8d59b46a363280f6d0051
github username <joanrigdon@gmail.com>
2019-11-05 19:21:12 -0500

    images are now rendering

--- src/features/resources/ResearchContainer.js
+++ src/features/resources/ResearchContainer.js
@@ -3,36 +3,40 @@ import { Container, Divider, Image } from 'semantic-ui-react'
 import { researchLinks } from './ResearchLinks'
 
 
+
+
 class ResearchContainer extends Component {
 
 
   render(){
 
     return (
-      <div>
-        <Container className="researchContainer">
-          {researchLinks.map(research =>
-            <div>
+      <div className="researchContainer">
+        <Container>
               <br/>
-              <Image src="research.image"/>
-              <div className="large resourceItemTitle">{research.title}</div>
-              <br/>
-              <div className="resourceItemSubtitle">{research.subtitle}</div>
+              {researchLinks.map(research =>
+              <>
+                <Image src={research.image}/>
+                <div className="large resourceItemTitle">{research.title}</div>
+                <br/>
+                <div className="resourceItemSubtitle">{research.subtitle}</div>
 
-              <br/>
-              <br/>
+                <br/>
+                <br/>
 
-              <div className="right">
-                <button className="resourceItemButton"><a href={research.buttonLink}>Learn More</a></button>
-              </div>
-              <br/>
+                <div className="right">
+                  <button className="resourceItemButton"><a href={research.buttonLink}>Learn More</a></button>
+                </div>
+                <br/>
+              </>
+              )}
               <Divider />
 
-            </div>
+            </Container>
 
           )}
-        </Container>
-      </div>
+        </div>
+
 
     )
   }
--- src/features/resources/ResearchLinks.js
+++ src/features/resources/ResearchLinks.js
@@ -4,7 +4,7 @@ const buildingSkills = {
   subtitle: "World Bank Report",
   description: "This report is part of the Rapid Technology Skills Technical Assistance (“Decoding Bootcamps”) initiative by the World Bank, which aims to collect and share examples and lessons of bootcamps in emerging markets, and measure the impact of bootcamp training on youth employment in selected countries.",
   buttonLink: 'http://documents.worldbank.org/curated/en/795011502799283894/pdf/118627-WP-PUBLIC-P163475-78p-CodingBootcampsFutureProofSkills.pdf',
-  image: './codingBootcamp.jpg'
+  image: "https://tinyurl.com/yxo7dltn"
 }
 
 const guide = {
@@ -13,7 +13,7 @@ const guide = {
   subtitle: "World Bank Report",
   description: "This toolkit provides a step by step guide for policymakers and practitioners to develop coding bootcamp programs. The toolkit provides advice on what it takes to organize training from “scratch,” and hence help form an opinion on the ultimate model on which to settle.",
   buttonLink: "http://documents.worldbank.org/curated/en/175631522135001611/pdf/124627-WP-P163475-PUBLIC-CodingBootcampsToolkitMarch.pdf",
-  image: './guide.jpg'
+  image: 'https://image.isu.pub/170817211737-306e1b8aa5193db3d6400336a42da206/jpg/page_1.jpg'
 
 }
 
