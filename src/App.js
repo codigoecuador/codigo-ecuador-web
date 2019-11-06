c7b9def90732999d04bfcfd0815c7cc3613ddfb6
github username <joanrigdon@gmail.com>
2019-11-06 15:08:45 -0500

    removed header component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import React from "react"
 import "./App.css"
-import Header from './features/header/Header'
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+// import { HashLink as Link } from 'react-router-hash-link';
+import Navbar from "./features/navbar/Navbar"
+import Home from "./features/home/Home"
+import About from "./features/about/About"
+import Partners from "./features/about/partners/Partners"
+import Team from "./features/about/team/Team"
+import Blog from "./features/blog/Blog"
+import Donate from "./features/donate/Donate"
+import CodingContainer from "./features/resources/coding/CodingContainer"
+import ResearchContainer from './features/resources/research/ResearchContainer'
+import Activities from "./features/activities/Activities"
+import Logo from "./features/logo/Logo"
 
 
 
 function App() {
 
 	return (
-			<Header />
+		<Router>
+			<div>
+				<Navbar />
+				<Switch>
+					<Route exact path="/" render={() => <Home />}></Route>
+					<Route exact path="/about" render={() => <About />}></Route>
+					<Route exact path="/activities" render={() => <Activities />}></Route>
+					<Route exact path="/about/partners" render={() => <Partners />} ></Route>
+          <Route exact path="/about/team" render={() => <Team />} ></Route>
+					<Route exact path="/resources" render={() => <CodingContainer />}></Route>
+					<Route exact path="/resources/research" render={() => <ResearchContainer />}></Route>
+					<Route exact path="/blog" render={() => <Blog />}></Route>
+					<Route exact path="/donate" render={() => <Donate />}></Route>
+				</Switch>
+
+        <div className="banner">
+					<Logo className="logo" />
+				</div>
+			</div>
+    </Router>
 	)
 }
 
--- src/features/resources/research/ResearchCard.js
+++ src/features/resources/research/ResearchCard.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import { Card, Image } from "semantic-ui-react"
+import { Card  } from "semantic-ui-react"
 
 class ResearchCard extends Component {
 	render() {
@@ -7,20 +7,6 @@ class ResearchCard extends Component {
 
 		return (
 			<>
-				{/* <Card>
-					<Image src={research.image} className="researchImage" />
-
-					<Card.Content>
-						<Card.Header>
-							<div className="large">{research.title}</div>
-						</Card.Header>
-						<Card.Meta>{research.subtitle}</Card.Meta>
-
-						<Card.Description>{research.description}</Card.Description>
-					</Card.Content>
-
-					<Card.Content extra className="cardBottom"></Card.Content>
-        </Card> */}
 
 				<Card
 					image={research.image}
