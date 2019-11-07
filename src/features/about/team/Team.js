1f010f7a6198ae38789033b3787611563ebb0e37
github username <joanrigdon@gmail.com>
2019-11-07 02:11:21 -0500

    added under construction to team page

--- src/features/about/team/Team.js
+++ src/features/about/team/Team.js
@@ -5,6 +5,7 @@ import './Team.css'
 const Team = () => {
     return (
         <Container className="team center">
+            <h1>Under construction ...</h1>
             <p>Código Ecuador is developing through an extensive network of social activists and computer programming advocates. We’re very proud of the diversity of our partners, and their abilities to contribute their own unique experience and skills to our success. Find out more about some of our team members below.</p>
         </Container>
     )
