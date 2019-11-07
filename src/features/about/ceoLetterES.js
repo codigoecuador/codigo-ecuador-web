032810f034043ab751d65d735efd89203477ecea
github username <joanrigdon@gmail.com>
2019-11-07 10:51:20 -0500

    added ceo letter in spanish

---
+++ src/features/about/ceoLetterES.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const CeoLetter = () => {
+    return (
+        <div className="bubble">
+          <div className="ceo-letter-headline">
+            <span className="gold">Letter From</span><span className="navy"> Our CEO</span>
+          </div>
+            <i className="ceo-letter-text">
+              <p>
+              Estimados socios y amigos,
+              </p>
+
+              <p>Gracias por tomarse el tiempo para aprender más sobre nuestra organización. Código Ecuador ha sido un objetivo mío de alguna forma durante más de 12 años. Al ingresar a la vida adulta con apenas 18 años, comencé a experimentar lo difícil que es lograr y alcanzar objetivos. Si no fuera por mi experiencia de primera mano al tratar de pagar el alquiler y hacer malabares con el trabajo y la escuela, fue por las personas que conocí en mi comunidad que lucharon por mantener a sus familias y vivir una vida plena.</p>          ​
+
+              <p>No estaba en mi naturaleza saber que lucharon y no ofrecieron mi apoyo. Probablemente intenté ayudar a demasiadas personas a una edad tan temprana. Desde la madre soltera que se recuperaba del alcoholismo y que necesitaba a alguien que la ayudara a buscar trabajo y la llevara al trabajo hasta adolescentes más jóvenes confundidas porque sus padres bebían y consumían drogas, estuve allí para ellos porque sabía que había una brecha en los servicios disponibles. Quería servirles y hacerles la vida un poco más fácil.</p>          ​
+
+              <p>Solo me volví más empático con estas dificultades mientras continuaba creciendo y experimentando las duras realidades de la vida. Tomé un camino diferente al de muchas otras mujeres en mi posición. Es más fácil para dos personas apoyarse mutuamente en una relación que para alguien soltero y sin la ayuda de un cónyuge o pareja. Decidí terminar la universidad como soltera porque no quería quedarme en una relación por conveniencia. Muchas mujeres de bajos ingresos no tienen más remedio que entablar relaciones con hombres y quedarse con ellas por miedo a perder esa red de seguridad.</p>
+              ​
+              <p>No fue un camino fácil el que tomé. Es por eso que quiero conectar a las mujeres, en particular, con los recursos educativos que les ayudarán a tener éxito. Con el advenimiento de Internet, hay muchas maneras en que las personas pueden adquirir habilidades sin gastar una fortuna y endeudarse. Todo lo que necesitan hacer es abrir un navegador de búsqueda y encontrar habilidades de alto rendimiento para aprender de forma gratuita en la red mundial. Con nuestra ayuda en Código Ecuador, las mujeres de los países en desarrollo aprenderán sobre las opciones de educación no tradicional para ayudarlas a tener éxito en el mundo, solas, sin tener que comprometer sus valores.</p>    ​
+
+              <p>Gracias,</p>
+
+              <p>Debra Ray </p>
+
+              <p>Fundador y CEO de Código Ecuador</p>
+            </i>
+
+          <br/>
+        </div>
+    )
+}
+
+export default CeoLetter
