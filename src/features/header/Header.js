dc86e8f0ee0a6ccf758a35291fac983e139339d5
github username <joanrigdon@gmail.com>
2019-11-06 21:02:47 -0500

    made navbar responsive

--- src/features/header/Header.js
+++ src/features/header/Header.js
@@ -1,17 +1,52 @@
-import React from "react"
+import React, { Component } from "react"
 import Navbar from '../navbar/Navbar'
 import Logo from '../logo/Logo'
 
 
-const Header = () => {
-	return (
-		<>
-			<Navbar/>
-      <div className="banner">
-        <Logo className="logo" />
-      </div>
-		</>
-	)
+class Header extends Component  {
+  constructor(props) {
+  super()
+
+    this.state = {
+        width: window.innerWidth
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleWindowSizeChange);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowSizeChange);
+  }
+
+  handleWindowSizeChange = () => {
+    this.setState({ width: window.innerWidth });
+  }
+
+  render(){
+    let isMobile
+    const width = this.state.width
+    width <= 1100 ? isMobile = true : isMobile = false
+
+    let navStyle
+
+    let header
+    isMobile ? navStyle = "navbar navbar-mobile" : navStyle = "navbar navbar-desktop"
+
+
+  	return (
+  		<>
+        <div className={navStyle}>
+  			   <Navbar/>
+        </div>
+
+        <div className="banner">
+          <Logo className="logo" />
+        </div>
+  		</>
+  	)
+  }
 }
 
 export default Header
--- src/features/navbar/Navbar.js
+++ src/features/navbar/Navbar.js
@@ -1,31 +1,61 @@
-import React from "react"
-import "./NavBar.css"
+import React, { Component } from "react"
+import "./Navbar.css"
 import { Container } from "semantic-ui-react"
 import { Link } from "react-router-dom"
 
-const Navbar = () => {
-	return (
-		<Container className="navbar">
-			<Link className="active" to="/">
-				Home
-			</Link>
-			<Link exact className="active" to="/about">
-				About
-			</Link>
-			<Link exact className="active" to="/activities">
-				Activities
-			</Link>
-			<Link exact className="active" to="/resources">
-				Resources
-			</Link>
-			<Link exact className="active" to="/blog">
-				Blog
-			</Link>
-			<Link exact className="active" to="/donate">
-				Donate
-			</Link>
-		</Container>
-	)
+class Navbar extends Component {
+	constructor(props) {
+	super()
+
+		this.state = {
+				width: window.innerWidth
+			}
+		}
+
+		componentDidMount() {
+			window.addEventListener('resize', this.handleWindowSizeChange);
+		}
+
+		componentWillUnmount() {
+			window.removeEventListener('resize', this.handleWindowSizeChange);
+		}
+
+		handleWindowSizeChange = () => {
+			this.setState({ width: window.innerWidth });
+		}
+
+		render(){
+			let isMobile
+			const width = this.state.width
+			width <= 760 ? isMobile = true : isMobile = false
+
+			let buttonStyle
+
+			isMobile ? buttonStyle = "nav-button-active nav-button-mobile" : buttonStyle = "nav-button-active nav-button-desktop"
+
+		return (
+			<Container>
+				<Link className={buttonStyle} to="/">
+					Home
+				</Link>
+				<Link exact className={buttonStyle} to="/about">
+					About
+				</Link>
+				<Link exact className={buttonStyle} to="/activities">
+					Activities
+				</Link>
+				<Link exact className={buttonStyle} to="/resources">
+					Resources
+				</Link>
+				<Link exact className={buttonStyle} to="/blog">
+					Blog
+				</Link>
+				<Link exact className={buttonStyle} to="/donate">
+					Donate
+				</Link>
+			</Container>
+		)
+	}
 }
 
 export default Navbar
