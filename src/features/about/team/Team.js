a7ed6a8c55be40447bc5a29fc2eeb04a086b049e
AudreyJones <audrey.eliz.jones@gmail.com>
2019-11-08 13:15:13 -0500

    Working with ui fluid card group for Team Page

--- src/features/about/team/Team.js
+++ src/features/about/team/Team.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react'
-import { Container, Card, Image, Divider } from "semantic-ui-react"
+import { Container, Card, Divider, Grid } from "semantic-ui-react"
 import './Team.css'
-// import Pic from '###'
 import {teamData} from './TeamData'
 import TeamCard from './TeamCard'
 
@@ -10,28 +9,32 @@ class Team extends Component {
         // debugger
     return (
         
-        <Container className="team center">
+        <Container className="team center" justified>
+                    {/* <br /> */}
                 <div className="headline">
 					<span className="gold">Our Team</span>
 				</div>
-                {/* <Image src={Pic} fluid/> */}
                 <div className="subhead">
                 Código Ecuador is developing through an extensive network of social activists and computer programming advocates. We’re very proud of the diversity of our partners, and their abilities to contribute their own unique experience and skills to our success. Find out more about some of our team members below.
 				</div>
-                <br />
+                    <br />
                 <Divider/>
-                <br/>
+                    <br/>
                 <div className="headline">
 					<span className="navy">Meet the Team</span>
 				</div>
-                <br />
-                <Card.Group itemsPerRow={2} className="center">
-                    {teamData.map(member => 
-                    
-                        <TeamCard {...member}  />
-                    
-                    )}
-                </Card.Group>
+                    <br />
+                <div class="ui grid" columns={3} fluid>
+                {/* <Grid class="ui grid" padded columns={3} fluid> */}
+                    <div className='ui fluid cards' justified fluid columns={3}>
+                    <Card.Group centered fluid justified>
+                        {teamData.map(member => 
+                            <TeamCard {...member} />
+                        )}
+                    </Card.Group>
+                    </div>
+                {/* </Grid> */}
+                </div>
             
         </Container>
         
--- src/features/about/team/TeamCard.js
+++ src/features/about/team/TeamCard.js
@@ -1,34 +1,34 @@
 import React from 'react'
-import { Card } from 'semantic-ui-react'
+import { Card, Feed, Image } from 'semantic-ui-react'
 
 
 
-const TeamCard = ({name, title, description, background, image}) => {
+const TeamCard = ({name, title, linkedin, description, background, image}) => {
 
     return (
-      <div className='team-cards-group'>
+      
 
-          <Card>
+          <Card 
+            href={linkedin}
+          raised fluid>
+              <Image src='###' alt='Profile Pic'/>
               <Card.Content>
-                <Card.Header>
-                    <p>Profile Picture</p>
-                    {/* {image} */}
+                <Card.Header>{name}
                 </Card.Header>
                 <Card.Description>
-                    <h3>{name}</h3>
-                    <h6>{title}</h6>
-                    <p>{description}</p>
-                    <p>{background}</p>
+                {title}
                 </Card.Description>
-                </Card.Content>
+            </Card.Content>
+            <Card.Content>
+                <Feed>
+                    <Feed.Event>{description}</Feed.Event>
+                    <Feed.Event>{background}</Feed.Event>
+                </Feed>
+            </Card.Content>
+            <Card.Content extra>Find me on LinkedIn</Card.Content>
           </Card>
 
-
-        <br/>
-        <br/>
-        <br/>
-        <br/>
-      </div>
+      
     )
 }
 
