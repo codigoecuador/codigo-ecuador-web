0ba0053732d12c4a37536accdb6ca3fa8471092d
github username <joanrigdon@gmail.com>
2019-11-12 10:43:11 -0500

    restored styling for classes

--- src/features/classes/Classes.js
+++ src/features/classes/Classes.js
@@ -1,4 +1,5 @@
 import React,  { Component } from 'react'
+import { connect } from 'react-redux'
 import { Container, Divider } from "semantic-ui-react"
 import './Classes.css'
 import { pythonClasses } from './PythonData'
@@ -6,10 +7,12 @@ import { text } from  './pythonClassesText.js'
 
 class Classes extends Component {
   render(){
+    let coursesStyle
+    this.props.size === "mobile" ? coursesStyle = "courses-mobile" : coursesStyle = "courses-desktop"
 
 
     return (
-      <Container className="courses">
+      <Container className={coursesStyle}>
         <div className="courses-text">
           <div className="center headline">Our Curriculum</div>
           {text}
@@ -50,4 +53,7 @@ class Classes extends Component {
 
 }
 
-export default Classes
+
+const mapStateToProps = state => {  return {  size: state.size  } }
+
+export default connect(mapStateToProps)(Classes)
