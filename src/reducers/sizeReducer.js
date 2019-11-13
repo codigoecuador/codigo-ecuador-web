a052b482abf21325d3086d395f8212bbeb1de704
github username <joanrigdon@gmail.com>
2019-11-13 12:27:49 -0500

    added size-reducer

---
+++ src/reducers/sizeReducer.js
@@ -0,0 +1,12 @@
+export default function rootReducer (state = {size: ""}, action) { 
+
+  switch(action.type){
+    case "SET_SIZE": 
+      let x = {...state, size: action.setSize} 
+      return x;
+
+
+      default:
+        return state
+
+} }
