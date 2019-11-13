6c356926466c300d5e0503f617f430c08a80863c
github username <joanrigdon@gmail.com>
2019-11-13 12:18:39 -0500

    separated reducers into function / language, size

--- src/rootReducer.js
+++
@@ -1,14 +0,0 @@
-
-
-export default function rootReducer (state = {size: ""}, action) { 
-
-  switch(action.type){
-    case "SET_SIZE": 
-      let x = {...state, size: action.setSize} 
-      return x;
-
-
-      default:
-        return state
-
-} }
