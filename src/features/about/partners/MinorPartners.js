59c97b6050e4bb661cf0164eb0e515b4e7775c51
Lizzie <lchan217@gmail.com>
2019-11-16 16:14:22 -0700

    add minor partner cards

---
+++ src/features/about/partners/MinorPartners.js
@@ -0,0 +1,30 @@
+import React from "react"
+import { Container, Card, Link } from "semantic-ui-react"
+import { minorPartnersData } from "./MinorPartnersData"
+//notes from lizzie
+// become a partner - on website it has a form, but I just included their email
+// can play with amount of cards per row once we have total amount of donors
+
+const minorPartners = () => {
+	return (
+		<Container>
+				<Card.Group itemsPerRow={4} className="center">
+					{minorPartnersData.map(partner =>
+						<Card>
+							<div className="image">
+								<img src={partner.image} alt={`${partner.image}`} />
+							</div>
+							<div className="content">
+								<div className="header"><a style = {{ fontSize: 22, color: '#0000EE' }} href={partner.link} target="_blank" >{partner.name}</a></div>
+							</div>
+							<div className="description">
+								{partner.content}
+							</div>
+						</Card>
+					)}
+				</Card.Group>
+		</Container>
+	)
+}
+
+export default minorPartners
