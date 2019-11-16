75a435eb9065a7489c5690526a9ba76d372102ea
Lizzie <lchan217@gmail.com>
2019-11-16 15:35:07 -0700

    add become a partner to minorPData

--- src/features/about/partners/MinorPartnersData.js
+++ src/features/about/partners/MinorPartnersData.js
@@ -1,5 +1,13 @@
 import kodluyoruzLogo  from "./kodluyoruz.webp"
 
+const partner1 = {
+	name: "Become a Partner" ,
+	image: 'https://static.wixstatic.com/media/nsplsh_54616d4d6272346f6b7634~mv2_d_6016_4016_s_4_2.jpg/v1/fill/w_246,h_164,al_c,q_80,usm_0.66_1.00_0.01,blur_2/nsplsh_54616d4d6272346f6b7634~mv2_d_6016_4016_s_4_2.jpg',
+	content: [
+		'If you are looking for talent or want to have a partner available to share best practices and resources, please feel free to reach out to us with your information. We are always looking to form new connections with businesses and organizations that share our values.'
+	]
+}
+
 const kod = {
 	name: "Kodluyoruz",
 	image: kodluyoruzLogo,
@@ -7,14 +15,6 @@ const kod = {
 		"Kodluyoruz.org is a software movement that promotes code literacy and adds value to the future of young people in the software field in order to improve every segment of society by using technology."
 	]
 }
-const partner2 = {
-	name: "Partner 2",
-	image: 'image2',
-	content: [
-		"Here is some information about a potential partner."
-	]
-}
-
 const partner3 = {
 	name: "Partner 3",
 	image: 'image3',
@@ -23,4 +23,12 @@ const partner3 = {
 	]
 }
 
-export const minorPartnersData = [kod, partner2, partner3]
+const partner4 = {
+	name: "Partner 4",
+	image: 'image4',
+	content: [
+		"Here is some information about a potential partner."
+	]
+}
+
+export const minorPartnersData = [partner1, kod, partner3, partner4]
