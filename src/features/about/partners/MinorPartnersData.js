8e04f798f02c36592215b8eb3c279d5ef585eed9
Lizzie <lchan217@gmail.com>
2019-11-16 15:05:33 -0700

    add minor partner test data

--- src/features/about/partners/MinorPartnersData.js
+++ src/features/about/partners/MinorPartnersData.js
@@ -7,5 +7,20 @@ const kod = {
 		"Kodluyoruz.org is a software movement that promotes code literacy and adds value to the future of young people in the software field in order to improve every segment of society by using technology."
 	]
 }
+const partner2 = {
+	name: "Partner 2",
+	image: 'image2',
+	content: [
+		"Here is some information about a potential partner."
+	]
+}
 
-export const minorPartnersData = [kod]
+const partner3 = {
+	name: "Partner 3",
+	image: 'image3',
+	content: [
+		"Here is some information about a potential partner."
+	]
+}
+
+export const minorPartnersData = [kod, partner2, partner3]
