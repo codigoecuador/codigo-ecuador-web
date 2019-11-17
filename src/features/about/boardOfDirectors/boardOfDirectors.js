5a6882d28926510ead6ce1f7ca068beaab89ee15
Lizzie <lchan217@gmail.com>
2019-11-16 17:55:19 -0700

    add styling to cards

--- src/features/about/boardOfDirectors/boardOfDirectors.js
+++ src/features/about/boardOfDirectors/boardOfDirectors.js
@@ -16,29 +16,29 @@ class BoardOfDirectors extends Component {
 
             <Card>
               <div class="content">
-                <h1>Debra Ray</h1>
+                <h1 className="board-header">Debra Ray</h1>
                 <div class="meta">
-                  <span>Founder and CEO</span>
+                  <span className="board-meta">Founder and CEO</span>
                 </div>
-                <div class="description">
+                <div class="description board-description">
                 I want to advance global economic development by sharing resources with women entering adulthood to help them become more independent.
                 </div>
               </div>
             </Card>
             <Card>
               <div class="content">
-                <h1>Tom Sullivan</h1>
+                <h1 className="board-header">Tom Sullivan</h1>
                 <div class="meta">
-                  <span>President</span>
+                  <span className="board-meta">President</span>
                 </div>
-                <div class="description">
+                <div class="description board-description">
                 I want to help the youth of tomorrow turn their creative ideas into reality with computer programming.
                 </div>
               </div>
             </Card>
 
             <Card>
-              <img style={{width: 420, height: 422, display: "block", margin: '0 auto'}}src="https://static.wixstatic.com/media/47ab14_85e2c9c81a0a45b6b2b6ee0623d1f9f3~mv2.jpg/v1/fill/w_420,h_442,al_c,q_80,usm_0.66_1.00_0.01/Image%20from%20iOS_edited.webp" />
+              <img style={{width: 420, height: 422, display: "block", margin: '0 auto'}} src="https://static.wixstatic.com/media/47ab14_85e2c9c81a0a45b6b2b6ee0623d1f9f3~mv2.jpg/v1/fill/w_420,h_442,al_c,q_80,usm_0.66_1.00_0.01/Image%20from%20iOS_edited.webp" />
             </Card>
           </Card.Group>
       </Container>
