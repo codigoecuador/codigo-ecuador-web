eab5220b3ea216343e4c9040da99662883bc3d6d
kirstenwerner <kirstenmwerner@gmail.com>
2019-11-22 15:06:32 -0800

    handled lang for services container

--- src/features/services/Services.js
+++ src/features/services/Services.js
@@ -1,79 +1,80 @@
-import React, { Component } from "react"
-import { connect } from 'react-redux'
-import './Services.css'
-import { Container } from "semantic-ui-react"
-import Classes from './classes/Classes'
-import Mentorship from './mentorship/Mentorship'
-
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./Services.css";
+import { Container } from "semantic-ui-react";
+import { servicesContainerText } from "../../common/servicesText/servicesContainerText";
+import Classes from "./classes/Classes";
+import Mentorship from "./mentorship/Mentorship";
 
 class Services extends Component {
   state = {
     showing: ""
-  }
+  };
 
   handleClick = event => {
-    this.setState({ showing: event.target.value })
-  }
+    this.setState({ showing: event.target.value });
+  };
 
-
-  render(){
-
-    let showing
-    if(this.state.showing === "classes"){
-      showing = <Classes />
+  render() {
+    let showing;
+    if (this.state.showing === "classes") {
+      showing = <Classes />;
     } else {
-      showing = <Mentorship />
+      showing = <Mentorship />;
     }
 
-    let buttonGroup
-    let classesButtonStyle
-    let mentorButtonStyle
-    let servicesShowing
+    let buttonGroup;
+    let classesButtonStyle;
+    let mentorButtonStyle;
+    let servicesShowing;
 
-//need to DRY this up
-    if(this.props.size === "mobile"){
-      buttonGroup = "services-button-group-mobile"
-      classesButtonStyle = "services-button-mobile classes-button"
-      mentorButtonStyle = "services-button-mobile mentorship-button"
-      servicesShowing = "services-showing-mobile"
+    //need to DRY this up
+    if (this.props.size === "mobile") {
+      buttonGroup = "services-button-group-mobile";
+      classesButtonStyle = "services-button-mobile classes-button";
+      mentorButtonStyle = "services-button-mobile mentorship-button";
+      servicesShowing = "services-showing-mobile";
     } else {
-      buttonGroup = "services-button-group-desktop"
-      classesButtonStyle = "services-button-desktop classes-button"
-      mentorButtonStyle = "services-button-desktop mentorship-button"
-      servicesShowing = "services-showing-desktop"
+      buttonGroup = "services-button-group-desktop";
+      classesButtonStyle = "services-button-desktop classes-button";
+      mentorButtonStyle = "services-button-desktop mentorship-button";
+      servicesShowing = "services-showing-desktop";
     }
-
-
-
 
     return (
-
-			<Container className="services-container">
+      <Container className='services-container'>
         <div className={buttonGroup}>
-          <button className={classesButtonStyle} onClick={this.handleClick} value="classes">
-            Classes
+          <button
+            className={classesButtonStyle}
+            onClick={this.handleClick}
+            value='classes'
+          >
+            {
+              servicesContainerText[localStorage.getItem("language")]
+                .classesButton
+            }
           </button>
 
-          <button className={mentorButtonStyle} onClick={this.handleClick} value="mentorship">
-            Mentorship
+          <button
+            className={mentorButtonStyle}
+            onClick={this.handleClick}
+            value='mentorship'
+          >
+            {
+              servicesContainerText[localStorage.getItem("language")]
+                .mentorshipButton
+            }
           </button>
         </div>
 
-        <div className={servicesShowing}>
-          {showing}
-        </div>
-
-			</Container>
-
-		)
-
-
+        <div className={servicesShowing}>{showing}</div>
+      </Container>
+    );
   }
-
-
 }
 
+const mapStateToProps = state => {
+  return { language: state.language };
+};
 
-const mapStateToProps = state => {  return {  size: state.size  } }
-
-export default connect(mapStateToProps)(Services)
+export default connect(mapStateToProps)(Services);
