a9e6b94301ab2cbff15d69bb21cec3337e9b36ba
bebopvinh <vinhdo07@gmail.com>
2019-11-22 14:54:51 -0800

    merged objects under educatorLinks with two keys: EN, ES

--- src/features/resources/coding/EducatorLinks.js
+++ src/features/resources/coding/EducatorLinks.js
@@ -1,31 +1,75 @@
-const codeCombat = {
-  id: 1,
-  title: "CodeCombat for Teachers",
-  subtitle: "Helps educators teach students how to code in either JavaScript or Python, the most popular coding languages.",
-  buttonLink: 'https://codecombat.com/'
+export const educatorLinks = {
+  EN: [
+    {
+      id: 1,
+      name: "codeCombat",
+      title: "CodeCombat for Teachers",
+      subtitle:
+        "Helps educators teach students how to code in either JavaScript or Python, the most popular coding languages.",
+      buttonLink: "https://codecombat.com/",
+    },
+
+    {
+      id: 2,
+      name: "google",
+      title: "Google Resources for Educators",
+      subtitle:
+        "Code with Google provides technical curriculum for classrooms no matter what the students' future goals may be.",
+      buttonLink:
+        "https://edu.google.com/code-with-google/?modal_active=none&story-card_activeEl=enhance-any-subject",
+    },
+    {
+      id: 3,
+      name: "mobile",
+      title: "Mobile Computer Science Principles Curriculum",
+      subtitle:
+        "An NSF-funded effort to introduce computer science curricula based on a coding language to create mobile apps.",
+      buttonLink: "https://course.mobilecsp.org/mobilecsp/course",
+    },
+    {
+      id: 4,
+      name: "apple",
+      title: "Apple Teacher Guides",
+      subtitle:
+        "Downloadable coding curriculum built by the leading technology firm, Apple.",
+      buttonLink: "https://www.apple.com/education/teaching-code/",
+    },
+  ],
+  //spanish
+  ES: [
+    {
+      id: 1,
+      name: "codeCombat",
+      title: "Translate: CodeCombat for Teachers",
+      subtitle:
+        "Translate: Helps educators teach students how to code in either JavaScript or Python, the most popular coding languages.",
+      buttonLink: "https://codecombat.com/",
+    },
+
+    {
+      id: 2,
+      name: "google",
+      title: "Translate: Google Resources for Educators",
+      subtitle:
+        "Translate: Code with Google provides technical curriculum for classrooms no matter what the students' future goals may be.",
+      buttonLink:
+        "https://edu.google.com/code-with-google/?modal_active=none&story-card_activeEl=enhance-any-subject",
+    },
+    {
+      id: 3,
+      name: "mobile",
+      title: "Translate: Mobile Computer Science Principles Curriculum",
+      subtitle:
+        "Translate: An NSF-funded effort to introduce computer science curricula based on a coding language to create mobile apps.",
+      buttonLink: "https://course.mobilecsp.org/mobilecsp/course",
+    },
+    {
+      id: 4,
+      name: "apple",
+      title: "Translate: Apple Teacher Guides",
+      subtitle:
+        "Translate: Downloadable coding curriculum built by the leading technology firm, Apple.",
+      buttonLink: "https://www.apple.com/education/teaching-code/",
+    },
+  ],
 }
-
-const google = {
-  id: 2,
-  title: "Google Resources for Educators",
-  subtitle: "Code with Google provides technical curriculum for classrooms no matter what the students' future goals may be.",
-  buttonLink: "https://edu.google.com/code-with-google/?modal_active=none&story-card_activeEl=enhance-any-subject"
-}
-
-const mobile = {
-  id: 3,
-  title: "Mobile Computer Science Principles Curriculum",
-  subtitle: "An NSF-funded effort to introduce computer science curricula based on a coding language to create mobile apps.",
-  buttonLink:"https://course.mobilecsp.org/mobilecsp/course"
-  }
-
-const apple = {
-  id: 4,
-  title: "Apple Teacher Guides",
-  subtitle: "Downloadable coding curriculum built by the leading technology firm, Apple.",
-  buttonLink:"https://www.apple.com/education/teaching-code/"
-  }
-
-
-
-export const educatorLinks = [codeCombat, google, mobile, apple]
