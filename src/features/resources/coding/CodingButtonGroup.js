c8079bc43b75267a40727d28b61656587b1ef3f6
bebopvinh <vinhdo07@gmail.com>
2019-11-22 13:45:13 -0800

    Cleaner renderButtons()

--- src/features/resources/coding/CodingButtonGroup.js
+++ src/features/resources/coding/CodingButtonGroup.js
@@ -4,42 +4,40 @@ import "./Coding.css"
 import { Button, Sticky } from "semantic-ui-react"
 import { codingText } from "../../../common/resourcesText/codingText"
 
-const CodingButtonGroup = (props) => {
-	//grabs language from localStorage and select text from codingText object
-	const language = localStorage.getItem("language")
-	const buttons = codingText[language].buttons
+const CodingButtonGroup = props => {
+  //grabs language from localStorage and select text from codingText object
+  const language = localStorage.getItem("language")
+  const buttons = codingText[language].buttons
 
-	const renderButtons = (buttons) => {
-		return buttons.map((b) => {
-			return (
-				<button
-					onClick={props.handleClick}
-					className="coding-button"
-					value={b.name}
-				>
-					{b.text}
-				</button>
-			)
-		})
-	}
-	return (
-		<Sticky>
-			<div className="coding-button-group">
-				<br />
-				<br />
-				<Button.Group vertical>
-					{renderButtons(buttons)}
-					<br />
-					<br />
-				</Button.Group>
-			</div>
-		</Sticky>
-	)
+  const renderButtons = buttons => {
+    return buttons.map(b => (
+      <button
+        onClick={props.handleClick}
+        className="coding-button"
+        value={b.name}
+      >
+        {b.text}
+      </button>
+    ))
+  }
+  return (
+    <Sticky>
+      <div className="coding-button-group">
+        <br />
+        <br />
+        <Button.Group vertical>
+          {renderButtons(buttons)}
+          <br />
+          <br />
+        </Button.Group>
+      </div>
+    </Sticky>
+  )
 }
-const mapStateToProps = (state) => {
-	return {
-		language: state.language
-	}
+const mapStateToProps = state => {
+  return {
+    language: state.language,
+  }
 }
 
 export default connect(mapStateToProps)(CodingButtonGroup)
