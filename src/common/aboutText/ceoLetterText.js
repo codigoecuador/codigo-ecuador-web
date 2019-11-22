3b901e24c6fbb1965828e232d8d18d727913ef38
kirstenwerner <kirstenmwerner@gmail.com>
2019-11-22 15:01:11 -0800

    created CEO letter text file

---
+++ src/common/aboutText/ceoLetterText.js
@@ -0,0 +1,118 @@
+import React from "react";
+
+export const ceoLetterText = {
+  EN: {
+    goldTitle: "Letter From",
+    navyTitle: " Our CEO",
+    text: (
+      <div>
+        <p>Dear Partners and Friends,</p>
+        <p>
+          Thank you for taking the time to learn more about our organization.
+          Código Ecuador has been a goal of mine in some form for over 12 years.
+          Upon just entering adult life at barely 18 years old, I began
+          experiencing how difficult it is to make and achieve goals. If it
+          wasn't for my first-hand experience in trying to pay rent and juggle
+          work and school, it was for the people I knew in my community who
+          struggled to support their families and live a fulfilling life.
+        </p>{" "}
+        ​
+        <p>
+          It wasn't in my nature to know that they struggled and not offer my
+          support. I probably tried to help too many people at such a young age.
+          From the single mother recovering from alcoholism who needed someone
+          to help her look for a job and drive her to work to younger teens
+          confused because their parents drank and did drugs - I was there for
+          them because I knew there was a gap in available services. I wanted to
+          serve them and make life a little bit easier for them.
+        </p>{" "}
+        ​
+        <p>
+          I only became more empathetic to these hardships as I continued to
+          grow up and experience the harsh realities of life. I took a path
+          different than many other women in my position. It is easier for two
+          people to support each other in a relationship than for someone to be
+          single and without the help of a spouse or partner. I made a choice to
+          attempt finishing college as a single woman because I didn't want to
+          stay in a relationship out of convenience. Many low-income women have
+          no choice but to enter into relationships with men and stay with them
+          out of fear of losing that safety net.
+        </p>
+        ​
+        <p>
+          It wasn't an easy path that I took. That is why I want to connect
+          women, in particular, to educational resources that will help them
+          succeed. With the advent of the internet, there are many ways for
+          people to gain skills without spending a fortune and getting
+          themselves into debt. All they need to do is open up a search browser
+          and find high return skills to learn for free on the world wide web.
+          With our help at Código Ecuador, women in developing countries will
+          learn about non-traditional education options to help them succeed in
+          the world - on their own, without having to compromise their values.
+        </p>{" "}
+        ​<p>Thank you,</p>
+        <p>Debra Ray </p>
+        <p>Founder and CEO of Código Ecuador</p>
+      </div>
+    )
+  },
+
+  ES: {
+    goldTitle: "Carta de",
+    navyTitle: " Nuestro CEO",
+    text: (
+      <div>
+        <p>Estimados socios y amigos,</p>
+        <p>
+          Gracias por tomarse el tiempo para aprender más sobre nuestra
+          organización. Código Ecuador ha sido un objetivo mío de alguna forma
+          durante más de 12 años. Al ingresar a la vida adulta con apenas 18
+          años, comencé a experimentar lo difícil que es lograr y alcanzar
+          objetivos. Si no fuera por mi experiencia de primera mano al tratar de
+          pagar el alquiler y hacer malabares con el trabajo y la escuela, fue
+          por las personas que conocí en mi comunidad que lucharon por mantener
+          a sus familias y vivir una vida plena.
+        </p>{" "}
+        ​
+        <p>
+          No estaba en mi naturaleza saber que lucharon y no ofrecieron mi
+          apoyo. Probablemente intenté ayudar a demasiadas personas a una edad
+          tan temprana. Desde la madre soltera que se recuperaba del alcoholismo
+          y que necesitaba a alguien que la ayudara a buscar trabajo y la
+          llevara al trabajo hasta adolescentes más jóvenes confundidas porque
+          sus padres bebían y consumían drogas, estuve allí para ellos porque
+          sabía que había una brecha en los servicios disponibles. Quería
+          servirles y hacerles la vida un poco más fácil.
+        </p>{" "}
+        ​
+        <p>
+          Solo me volví más empático con estas dificultades mientras continuaba
+          creciendo y experimentando las duras realidades de la vida. Tomé un
+          camino diferente al de muchas otras mujeres en mi posición. Es más
+          fácil para dos personas apoyarse mutuamente en una relación que para
+          alguien soltero y sin la ayuda de un cónyuge o pareja. Decidí terminar
+          la universidad como soltera porque no quería quedarme en una relación
+          por conveniencia. Muchas mujeres de bajos ingresos no tienen más
+          remedio que entablar relaciones con hombres y quedarse con ellas por
+          miedo a perder esa red de seguridad.
+        </p>
+        ​
+        <p>
+          No fue un camino fácil el que tomé. Es por eso que quiero conectar a
+          las mujeres, en particular, con los recursos educativos que les
+          ayudarán a tener éxito. Con el advenimiento de Internet, hay muchas
+          maneras en que las personas pueden adquirir habilidades sin gastar una
+          fortuna y endeudarse. Todo lo que necesitan hacer es abrir un
+          navegador de búsqueda y encontrar habilidades de alto rendimiento para
+          aprender de forma gratuita en la red mundial. Con nuestra ayuda en
+          Código Ecuador, las mujeres de los países en desarrollo aprenderán
+          sobre las opciones de educación no tradicional para ayudarlas a tener
+          éxito en el mundo, solas, sin tener que comprometer sus valores.
+        </p>{" "}
+        ​<p>Gracias,</p>
+        <p>Debra Ray </p>
+        <p>Fundador y CEO de Código Ecuador</p>
+      </div>
+    )
+  }
+};
