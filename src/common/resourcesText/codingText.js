0a3e9a2b361eb0df5e4c58b1385b49cb33292ab0
bebopvinh <vinhdo07@gmail.com>
2019-11-22 13:21:11 -0800

    add text and localization placeholders for resources buttons

---
+++ src/common/resourcesText/codingText.js
@@ -0,0 +1,31 @@
+export const codingText = {
+	EN: {
+		buttons: [
+			{ name: "Student", text: "For Students" },
+			{ name: "Parent", text: "For Parents" },
+			{ name: "Educator", text: "For Educators" },
+			{ name: "Entrepreneur", text: "For Entrepreneurs" }
+		]
+	},
+
+	ES: {
+		buttons: [
+			{
+				name: "Student",
+				text: "para estudiantes"
+			},
+			{
+				name: "Parent",
+				text: "Para padres"
+			},
+			{
+				name: "Educator",
+				text: "para educadores"
+			},
+			{
+				name: "Entrepreneur",
+				text: "para emprendedores"
+			}
+		]
+	}
+}
