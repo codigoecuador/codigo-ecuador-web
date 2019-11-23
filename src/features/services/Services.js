76e8492dd37258390dc2d693e02d6742cf07a99f
bebopvinh <vinhdo07@gmail.com>
2019-11-22 16:23:25 -0800

    Moved if/else for Classes/Mentoring components
    from render() to renderShowing()

--- src/features/services/Services.js
+++ src/features/services/Services.js
@@ -1,53 +1,54 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import "./Services.css";
-import { Container } from "semantic-ui-react";
-import { servicesContainerText } from "../../common/servicesText/servicesContainerText";
-import Classes from "./classes/Classes";
-import Mentorship from "./mentorship/Mentorship";
+import React, { Component } from "react"
+import { connect } from "react-redux"
+import "./Services.css"
+import { Container } from "semantic-ui-react"
+import { servicesContainerText } from "../../common/servicesText/servicesContainerText"
+import Classes from "./classes/Classes"
+import Mentorship from "./mentorship/Mentorship"
 
 class Services extends Component {
   state = {
-    showing: ""
-  };
+    showing: "",
+  }
 
   handleClick = event => {
-    this.setState({ showing: event.target.value });
-  };
+    this.setState({ showing: event.target.value })
+  }
+
+  renderShowing = () => {
+    if (this.state.showing === "classes") {
+      return <Classes />
+    } else {
+      return <Mentorship />
+    }
+  }
 
   render() {
-    let showing;
-    if (this.state.showing === "classes") {
-      showing = <Classes />;
-    } else {
-      showing = <Mentorship />;
-    }
-
-    let buttonGroup;
-    let classesButtonStyle;
-    let mentorButtonStyle;
-    let servicesShowing;
+    let buttonGroup
+    let classesButtonStyle
+    let mentorButtonStyle
+    let servicesShowing
 
     //need to DRY this up
     if (this.props.size === "mobile") {
-      buttonGroup = "services-button-group-mobile";
-      classesButtonStyle = "services-button-mobile classes-button";
-      mentorButtonStyle = "services-button-mobile mentorship-button";
-      servicesShowing = "services-showing-mobile";
+      buttonGroup = "services-button-group-mobile"
+      classesButtonStyle = "services-button-mobile classes-button"
+      mentorButtonStyle = "services-button-mobile mentorship-button"
+      servicesShowing = "services-showing-mobile"
     } else {
-      buttonGroup = "services-button-group-desktop";
-      classesButtonStyle = "services-button-desktop classes-button";
-      mentorButtonStyle = "services-button-desktop mentorship-button";
-      servicesShowing = "services-showing-desktop";
+      buttonGroup = "services-button-group-desktop"
+      classesButtonStyle = "services-button-desktop classes-button"
+      mentorButtonStyle = "services-button-desktop mentorship-button"
+      servicesShowing = "services-showing-desktop"
     }
 
     return (
-      <Container className='services-container'>
+      <Container className="services-container">
         <div className={buttonGroup}>
           <button
             className={classesButtonStyle}
             onClick={this.handleClick}
-            value='classes'
+            value="classes"
           >
             {
               servicesContainerText[localStorage.getItem("language")]
@@ -58,7 +59,7 @@ class Services extends Component {
           <button
             className={mentorButtonStyle}
             onClick={this.handleClick}
-            value='mentorship'
+            value="mentorship"
           >
             {
               servicesContainerText[localStorage.getItem("language")]
@@ -67,14 +68,14 @@ class Services extends Component {
           </button>
         </div>
 
-        <div className={servicesShowing}>{showing}</div>
+        <div className={servicesShowing}>{this.renderShowing()}</div>
       </Container>
-    );
+    )
   }
 }
 
 const mapStateToProps = state => {
-  return { language: state.language };
-};
+  return { language: state.language }
+}
 
-export default connect(mapStateToProps)(Services);
+export default connect(mapStateToProps)(Services)
