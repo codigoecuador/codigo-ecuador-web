4314b976c0359483bafc4b7820c9027cb59c6973
Lizzie <lchan217@gmail.com>
2019-11-25 12:01:57 -0700

    resize logo image

--- src/features/logo/Logo.js
+++ src/features/logo/Logo.js
@@ -4,7 +4,7 @@ import codigoLogo from './codigoLogo.png'
 const Logo = () => {
     return (
       <div>
-        <img src={ codigoLogo } className="logo" alt="codigo-ecuador logo, an icon of a brain with the words Equidad Global Empodaramiento Femenino"/>
+        <img src={ codigoLogo } style={{width: 500, height: 90}} className="logo" alt="codigo-ecuador logo, an icon of a brain with the words Equidad Global Empodaramiento Femenino"/>
       </div>
     )
 }
