e57f405dddf40c7cb4905d590fdba7782a0eb04b
github username <joanrigdon@gmail.com>
2019-11-26 15:40:17 -0500

    changed coding container to functional component

--- src/features/home/AboutBlurb.js
+++ src/features/home/AboutBlurb.js
@@ -1,17 +1,18 @@
-import React from "react"
-import { aboutBlurbText } from "../../common/homeText/aboutBlurbText"
+import React from "react";
+import { aboutBlurbText } from "../../common/homeText/aboutBlurbText";
 
 const AboutBlurb = () => {
-	return (
-		<div className="bubble">
-			<br />
-			<div className="home-headline">
-				{aboutBlurbText[localStorage.getItem("language")].title}
-			</div>
+  return (
+    <div className="bubble">
+      <br />
+      <div className="home-headline">
+        {aboutBlurbText[localStorage.getItem("language")].title}
+      </div>
+      <div className="banana">Banana</div>
 
-			<p>{aboutBlurbText[localStorage.getItem("language")].text}</p>
-		</div>
-	)
-}
+      <p>{aboutBlurbText[localStorage.getItem("language")].text}</p>
+    </div>
+  );
+};
 
-export default AboutBlurb
+export default AboutBlurb;
--- src/features/resources/coding/CodingContainer.js
+++ src/features/resources/coding/CodingContainer.js
@@ -1,58 +1,51 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./Coding.css";
 import CodingLinks from "./CodingLinks";
 import { connect } from "react-redux";
 import CodingButtonGroup from "./CodingButtonGroup";
 import CodingMenu from "./CodingMenu";
 
-class CodingContainer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showing: ""
-    };
-  }
+const CodingContainer = props => {
+  const [showing, setShowing] = useState("");
 
-  handleClick = event => {
-    this.setState({ showing: event.target.value });
+  const handleClick = event => {
+    // this.setState({ showing: event.target.value });
+    setShowing(event.target.value);
   };
 
-  handleMobileClick = event => {
-    this.setState({ showing: event });
+  const handleMobileClick = event => {
+    // this.setState({ showing: event });
+    setShowing(event);
   };
 
-  render() {
-    let codingButtonGroup;
-    if (this.props.size === "mobile") {
-      codingButtonGroup = (
-        <CodingMenu
-          fluid
-          handleMobileClick={this.handleMobileClick}
-          showing={this.state.showing}
-        />
-      );
-    } else {
-      codingButtonGroup = (
-        <CodingButtonGroup handleClick={this.handleClick} showing='showing' />
-      );
-    }
-    return (
-      <div className='main-container'>
-        <div className='headline banner-headline'>
-          <span className='gold'>
-            {this.state.showing || "Coding"} Resources
-          </span>
-        </div>
-
-        {codingButtonGroup}
-
-        <div className='showing'>
-          <CodingLinks type={this.state.showing} />
-        </div>
-      </div>
+  let codingButtonGroup;
+  if (props.size === "mobile") {
+    codingButtonGroup = (
+      <CodingMenu
+        fluid
+        handleMobileClick={handleMobileClick}
+        showing={showing}
+      />
+    );
+  } else {
+    codingButtonGroup = (
+      <CodingButtonGroup handleClick={handleClick} showing="showing" />
     );
   }
-}
+  return (
+    <div className="main-container">
+      <div className="headline banner-headline">
+        <span className="gold">{showing || "Coding"} Resources</span>
+      </div>
+
+      {codingButtonGroup}
+
+      <div className="showing">
+        <CodingLinks type={showing} />
+      </div>
+    </div>
+  );
+};
 
 const mapStateToProps = state => {
   return { size: state.size.size };
