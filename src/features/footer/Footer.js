68d194dfa8d0ad775bea00e0847b1343066e6d19
burakkocak884 <burakkocak884@hotmail.com>
2019-11-26 11:57:00 -0500

    footer renders almost everywhere correctly

--- src/features/footer/Footer.js
+++ src/features/footer/Footer.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import "./Footer.css"
-
+import Contact from "../donate/contact/Contact"
 const Footer = () => {
     return (
         <div className='footer'>
-            <h1 className = 'footer-headline'>Contact Codigo Ecuador</h1>
-            <p className = 'footer-content'>Would you like to support our efforts?</p>
-            <p className = 'footer-content'>Get in touch with Código Ecuador to learn more about our work and how you can get involved.</p>
+           <Contact className="center" />
         </div>
     )
 }
