27fdbb1011daed307a12e7a5588366249efbfacd
kirstenwerner <kirstenmwerner@gmail.com>
2019-11-26 13:30:23 -0800

    formatting changes

--- src/features/about/OurStory.js
+++ src/features/about/OurStory.js
@@ -1,23 +1,23 @@
-import React from "react"
-import { ourStoryText } from "../../common/aboutText/ourStoryText"
+import React from "react";
+import { ourStoryText } from "../../common/aboutText/ourStoryText";
 
 const OurStory = () => {
-	return (
-		<div className="about-bubble">
-			<div className="headline our-story-headline">
-				<span className="gold">
-					{ourStoryText[localStorage.getItem("language")].goldTitle}
-				</span>
+  return (
+    <div className='about-bubble'>
+      <div className='headline our-story-headline'>
+        <span className='gold'>
+          {ourStoryText[localStorage.getItem("language")].goldTitle}
+        </span>
 
-				<span className="navy">
-					{ourStoryText[localStorage.getItem("language")].navyTitle}
-				</span>
-			</div>
-			{ourStoryText[localStorage.getItem("language")].text}
-			{ourStoryText[localStorage.getItem("language")].links}
-			<br />
-		</div>
-	)
-}
+        <span className='navy'>
+          {ourStoryText[localStorage.getItem("language")].navyTitle}
+        </span>
+      </div>
+      {ourStoryText[localStorage.getItem("language")].text}
+      {ourStoryText[localStorage.getItem("language")].links}
+      <br />
+    </div>
+  );
+};
 
-export default OurStory
+export default OurStory;
