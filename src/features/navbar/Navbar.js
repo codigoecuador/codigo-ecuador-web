db79bfba1538ca6ef4f046515cbeb500b2f46efc
github username <joanrigdon@gmail.com>
2019-11-25 21:46:41 -0500

    added back home button to navbar

--- src/features/navbar/Navbar.js
+++ src/features/navbar/Navbar.js
@@ -8,48 +8,52 @@ const Navbar = () => {
 
     return (
       <Container className="dropdown-button-group">
-      <div class="dropdown">
-        <button class="dropdown-button">About</button>
-        <div class="dropdown-content">
-          <a href='/about'>
-            Our Story
-          </a>
-          <a href='/about/partners'>
-            Partners
-          </a>
-          <a href= '/about/team'>
-            Team
-          </a>
-          <a href='/about/board'>
-            Board of Directors
-          </a>
+
+        <Link exact className="dropdown" to="/">
+          <button className="dropdown-button">Home</button>
+        </Link>
+        <div class="dropdown">
+          <button class="dropdown-button">About</button>
+          <div class="dropdown-content">
+            <a href='/about'>
+              Our Story
+            </a>
+            <a href='/about/partners'>
+              Partners
+            </a>
+            <a href= '/about/team'>
+              Team
+            </a>
+            <a href='/about/board'>
+              Board of Directors
+            </a>
+          </div>
         </div>
-      </div>
 
-      <div class="dropdown">
-        <button className="dropdown-button">Services</button>
-        <div class="dropdown-content">
-          <a href='/services/classes'>
-            Curriculum
-          </a>
-          <a href= '/services/mentorship'>
-            Mentorship
-          </a>
+        <div class="dropdown">
+          <button className="dropdown-button">Services</button>
+          <div class="dropdown-content">
+            <a href='/services/classes'>
+              Curriculum
+            </a>
+            <a href= '/services/mentorship'>
+              Mentorship
+            </a>
+          </div>
         </div>
-      </div>
 
 
-      <Link exact className="dropdown" to="/resources">
-        <button className="dropdown-button">Resources</button>
-      </Link>
+        <Link exact className="dropdown" to="/resources">
+          <button className="dropdown-button">Resources</button>
+        </Link>
 
-      <Link exact className="dropdown" to="/blog">
-        <button className="dropdown-button">Blog</button>
-      </Link>
+        <Link exact className="dropdown" to="/blog">
+          <button className="dropdown-button">Blog</button>
+        </Link>
 
-      <Link exact className="dropdown" to="/donate">
-        <button className="dropdown-button">Donate</button>
-      </Link>
+        <Link exact className="dropdown" to="/donate">
+          <button className="dropdown-button">Donate</button>
+        </Link>
 
       </Container>
     )
