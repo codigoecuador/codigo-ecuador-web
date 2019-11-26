da4567a8153b7204bffe48db16237644b4e4e04e
kirstenwerner <kirstenmwerner@gmail.com>
2019-11-26 13:24:53 -0800

    connected redux back into about features

--- src/features/about/About.js
+++ src/features/about/About.js
@@ -1,20 +1,24 @@
-import React from "react"
-import "./About.css"
-import OurStory from "./OurStory"
-import CeoLetter from "./ceoLetter"
+import React from "react";
+import { connect } from "react-redux";
+import "./About.css";
+import OurStory from "./OurStory";
+import CeoLetter from "./ceoLetter";
 
 const About = () => {
-	return (
-		<div className="about-container main-container">
-			<div className="our-story">
-				<OurStory />
-			</div>
+  return (
+    <div className='about-container main-container'>
+      <div className='our-story'>
+        <OurStory />
+      </div>
+      <div className='ceo-letter'>
+        <CeoLetter />
+      </div>
+    </div>
+  );
+};
 
-			<div className="ceo-letter">
-				<CeoLetter />
-			</div>
-		</div>
-	)
-}
+const mapStateToProps = state => {
+  return { language: state.language };
+};
 
-export default About
+export default connect(mapStateToProps)(About);
