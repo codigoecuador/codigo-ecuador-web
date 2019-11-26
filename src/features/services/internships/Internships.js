21ad7b9672ccdd0bb5463bf8060d7ca77004f186
kirstenwerner <kirstenmwerner@gmail.com>
2019-11-26 13:58:06 -0800

    connected internship container to redux

--- src/features/services/internships/Internships.js
+++ src/features/services/internships/Internships.js
@@ -1,15 +1,20 @@
 import React from "react";
+import { connect } from "react-redux";
 import "./Internships.css";
 
 const Internships = () => {
   return (
-    <div className="main-container">
-      <div className="headline banner-headline">
-        <span className="gold">Internships</span>
+    <div className='main-container'>
+      <div className='headline banner-headline'>
+        <span className='gold'>Internships</span>
       </div>
       <h1>Internships!</h1>
     </div>
   );
 };
 
-export default Internships;
+const mapStateToProps = state => {
+  return { language: state.language };
+};
+
+export default connect(mapStateToProps)(Internships);
