0193d9a03973f8267e05cbce5fb70c395e33ba54
github username <joanrigdon@gmail.com>
2019-11-27 03:21:13 -0500

    styled footer

--- src/features/donate/contact/social.js
+++ src/features/donate/contact/social.js
@@ -2,22 +2,27 @@ import React from "react";
 
 export const social = (
   <div className="social-text">
-    <a href="mailto:info@codigoecuador.com">
-      <i class="envelope outline icon"></i>info@codigoecuador.com
-    </a>
-    <br />
-    <br />
-    <a href="https://twitter.com/CodigoEC">
-      <i class="twitter icon"></i>@CodigoEC
-    </a>
+    <div className="email">
+      <a href="mailto:info@codigoecuador.com">
+        <i class="envelope outline icon"></i>info@codigoecuador.com
+      </a>
+    </div>
     <br />
     <br />
 
-    <a href="tel:+15712165441">
-      <i class="phone icon"></i>
-      +1 571-216-5441
-    </a>
+    <div className="twitter">
+      <a href="https://twitter.com/CodigoEC">
+        <i class="twitter icon"></i>@CodigoEC
+      </a>
+    </div>
     <br />
     <br />
+
+    <div className="phone">
+      <a href="tel:+15712165441">
+        <i class="phone icon"></i>
+        +1 571-216-5441
+      </a>
+    </div>
   </div>
 );
--- src/features/footer/Footer.js
+++ src/features/footer/Footer.js
@@ -1,7 +1,38 @@
 import React from "react";
 import "./Footer.css";
-import Contact from "../donate/contact/Contact";
+import { social } from "../donate/contact/social";
+import { Grid } from "semantic-ui-react";
+
 const Footer = () => {
-  return <div className="footer">footer</div>;
+  return (
+    <div className="footer">
+      <Grid.Row columns={3}>
+        <Grid.Column verticalAlign="center">
+          <div className="email">
+            <a href="mailto:info@codigoecuador.com">
+              <i class="envelope outline icon"></i>info@codigoecuador.com
+            </a>
+          </div>
+        </Grid.Column>
+
+        <Grid.Column verticalAlign="center">
+          <div className="twitter">
+            <a href="https://twitter.com/CodigoEC">
+              <i class="twitter icon"></i>@CodigoEC
+            </a>
+          </div>
+        </Grid.Column>
+
+        <Grid.Column verticalAlign="center">
+          <div className="phone">
+            <a href="tel:+15712165441">
+              <i class="phone icon"></i>
+              +1 571-216-5441
+            </a>
+          </div>
+        </Grid.Column>
+      </Grid.Row>
+    </div>
+  );
 };
 export default Footer;
