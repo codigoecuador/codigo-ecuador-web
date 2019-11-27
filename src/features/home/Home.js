91cf4c1ab71b224194b8abc1b24dec0d5262797d
github username <joanrigdon@gmail.com>
2019-11-26 23:24:54 -0500

    fixing merge conflicts in home.js

--- src/features/home/Home.js
+++ src/features/home/Home.js
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import React from "react";
 import { connect } from "react-redux";
 import "./Home.css";
@@ -8,10 +7,10 @@ import Activities from "./activities/Activities";
 
 const Home = () => {
   return (
-    <div className='home-container'>
+    <div className="home-container">
       <CoverPic />
 
-      <div className='about-blurb'>
+      <div className="about-blurb">
         <AboutBlurb />
       </div>
 
@@ -25,51 +24,3 @@ const mapStateToProps = state => {
 };
 
 export default connect(mapStateToProps)(Home);
-=======
-import React, { Component } from "react"
-import { connect } from 'react-redux'
-import "./Home.css"
-import AboutBlurb from "./AboutBlurb"
-import CoverPic from "./CoverPic"
-import Activities from "../activities/Activities"
-
-
-class Home extends Component {
-
-    render() {
-
-
-        return ( <
-            >
-
-            <
-            CoverPic / >
-
-            <
-            div className = "about-blurb" >
-            <
-            AboutBlurb / >
-            <
-            /div>
-
-            <
-            div >
-            <
-            Activities / >
-            <
-            /div>
-
-            <
-            />
-        )
-
-    }
-
-}
-
-const mapStateToProps = state => {  return {  size: state.size  } }
-
-export default connect(mapStateToProps)(Home)
-
-//this.state.language === "EN" ? about = <AboutBlurb /> : about = <AboutBlurbES />
->>>>>>> Stashed changes
