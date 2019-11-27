c5ec262ee7c771e08d2cccc0560064e958e90db6
github username <joanrigdon@gmail.com>
2019-11-27 15:35:46 -0500

    restyled footer

--- src/features/footer/Footer.js
+++ src/features/footer/Footer.js
@@ -1,28 +1,50 @@
 import React from "react";
+import { Popup, Divider } from "semantic-ui-react";
 import "./Footer.css";
 import { Grid } from "semantic-ui-react";
 
 const Footer = () => {
   return (
     <div className="footer center">
-      <div className="email">
+      <Divider />
+      <h3>Contact us</h3>
+      <span className="social email">
         <a href="mailto:info@codigoecuador.com">
-          <i class="envelope outline icon"></i>info@codigoecuador.com
+          <i class="envelope outline icon"></i>email{""}
         </a>
-      </div>
+      </span>
 
-      <div className="twitter">
+      <span className="social twitter">
         <a href="https://twitter.com/CodigoEC">
-          <i class="twitter icon"></i>@CodigoEC
+          <i class="twitter icon"></i>Twitter
         </a>
-      </div>
+      </span>
 
-      <div className="phone">
-        <a href="tel:+15712165441">
-          <i class="phone icon"></i>
-          +1 571-216-5441
-        </a>
-      </div>
+      <span className="social phone">
+        <Popup
+          content="+1 571.216.5441"
+          trigger={
+            <a href="tel:+15712165441">
+              <i class="phone icon"></i>
+              phone
+            </a>
+          }
+          position="bottom center"
+        />
+      </span>
+
+      <span className="social linked-in">
+        <Popup
+          content="Linked In"
+          trigger={
+            <a href="https://www.linkedin.com/company/codigo-ecuador">
+              <i class="linkedin icon"></i>
+              Linked In
+            </a>
+          }
+          position="bottom center"
+        />
+      </span>
     </div>
   );
 };
