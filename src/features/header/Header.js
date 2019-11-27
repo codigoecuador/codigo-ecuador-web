d0cc3adebfaf604a8c68d5ee1371446266c39f83
github username <joanrigdon@gmail.com>
2019-11-27 01:31:48 -0500

    changed header to render different navbars based on window size

--- src/features/header/Header.js
+++ src/features/header/Header.js
@@ -1,27 +1,43 @@
-import React from "react"
-import Navbar from "./navbar/Navbar"
-import Logo from "./logo/Logo"
-import LanguageToggle from "./LanguageToggle"
-import { Link } from "react-router-dom"
+import React from "react";
+import { connect } from "react-redux";
+import Navbar from "./navbar/Navbar";
+import Logo from "./logo/Logo";
+import LanguageToggle from "./LanguageToggle";
+import { Link } from "react-router-dom";
+import MobileNavbar from "./MobileNavbar";
 
-const Header = () => {
-	return (
-		<div className="header">
-			<div className='ui right item'>
-				<LanguageToggle />
-			</div>
-
-			<Link exact className="logo-wrapper" to="/">
-				<Logo className="logo" />
-			</Link>
-
-      <div className = "navbar" >
-        <Navbar / >
-      </div>
-
-
+const Header = props => {
+  let desktopNavbar = (
+    <div className="navbar">
+      <Navbar />
     </div>
   );
-}
 
-export default Header
+  let mobileNavbar = (
+    <div className="mobile-navbar">
+      <MobileNavbar />
+    </div>
+  );
+
+  let navbar = props.size === "mobile" ? mobileNavbar : desktopNavBar;
+
+  return (
+    <div className="header">
+      <div className="ui right item">
+        <LanguageToggle />
+      </div>
+
+      <Link exact className="logo-wrapper" to="/">
+        <Logo className="logo" />
+      </Link>
+
+      {navbar}
+    </div>
+  );
+};
+
+const mapStateToProps = state => {
+  return { size: state.size.size };
+};
+
+export default connect(mapStateToProps)(Header);
