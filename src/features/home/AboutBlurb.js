a303fb3a1170285d994f1ceb9a0a18dc52df8d8d
github username <joanrigdon@gmail.com>
2019-11-26 20:22:50 -0500

    removed banana div from home page

--- src/features/home/AboutBlurb.js
+++ src/features/home/AboutBlurb.js
@@ -8,7 +8,6 @@ const AboutBlurb = () => {
       <div className="home-headline">
         {aboutBlurbText[localStorage.getItem("language")].title}
       </div>
-      <div className="banana">Banana</div>
 
       <p>{aboutBlurbText[localStorage.getItem("language")].text}</p>
     </div>
