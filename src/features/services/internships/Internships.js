665eddab7332f4492eaf440e5a3fe3ae5f540959
github username <joanrigdon@gmail.com>
2019-11-27 01:08:13 -0500

    added spanish functionality to internships, but does not work for title

--- src/features/services/internships/Internships.js
+++ src/features/services/internships/Internships.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { connect } from "react-redux";
 import "./Internships.css";
+import { internshipsText } from "../../../common/servicesText/internshipsText";
 
-const Internships = () => {
+const Internships = props => {
   return (
     <div className="main-container">
       <div className="headline banner-headline">
-        <span className="gold">Internships</span>
+        <span className="gold">
+          {internshipsText[localStorage.getItem("language")].title}
+        </span>
       </div>
 
-      <div className="cod">internships</div>
+      <div>{internshipsText[localStorage.getItem("language")].text}</div>
     </div>
   );
 };
