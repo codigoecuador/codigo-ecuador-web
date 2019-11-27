b9478c10c890c2ac57c61df85386a505954b6168
github username <joanrigdon@gmail.com>
2019-11-26 20:39:26 -0500

    added clickable phone number to contact

--- src/features/donate/contact/Contact.js
+++ src/features/donate/contact/Contact.js
@@ -4,32 +4,38 @@ import "./Contact.css";
 const Contact = () => {
   return (
     <>
-      <div className='contact-us center'>Contact us if you can help!</div>
+      <div className="contact-us center">Contact us if you can help!</div>
       <br />
+
       <div className="social-text">
-        <b>Email:</b> info@codigoecuador.com
-        <a href='mailto:info@codigoecuador.com'>
-          <i class='envelope outline icon'></i>
+        <a href="mailto:info@codigoecuador.com">
+          <i class="envelope outline icon"></i>info@codigoecuador.com
         </a>
-
-        <br/>
-        <br/>
-
-        <b>Twitter:</b> @CodigoEC
-        <a href='https://twitter.com/CodigoEC'>
-          <i class='twitter icon'></i>
+        <br />
+        <br />
+        <a href="https://twitter.com/CodigoEC">
+          <i class="twitter icon"></i>@CodigoEC
         </a>
+        <br />
+        <br />
+
+        <a href="tel:+15712165441">
+          <i class="phone icon"></i>
+          +1 571-216-5441
+        </a>
+        <br />
+        <br />
       </div>
 
       <a
-        className='donate-button'
-        href='https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=info@codigoecuador.com&item_name=Imagina%20C%C3%B3digo%20Ecuador&currency_code=USD'
+        className="donate-button"
+        href="https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=info@codigoecuador.com&item_name=Imagina%20C%C3%B3digo%20Ecuador&currency_code=USD"
       >
         Donate Now
       </a>
       <br />
       <br />
-      <div className='center'>
+      <div className="non-profit center">
         Código Ecuador is a 501(C)(3) organization under the International
         Intellectual Exchange Initiative Inc.
       </div>
