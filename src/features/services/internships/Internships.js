81d591fa1c3adf59a9985bf8de75c10b8b92a6f0
github username <joanrigdon@gmail.com>
2019-11-27 00:50:23 -0500

    moved About to top level of about folder

--- src/features/services/internships/Internships.js
+++ src/features/services/internships/Internships.js
@@ -4,11 +4,12 @@ import "./Internships.css";
 
 const Internships = () => {
   return (
-    <div className='main-container'>
-      <div className='headline banner-headline'>
-        <span className='gold'>Internships</span>
+    <div className="main-container">
+      <div className="headline banner-headline">
+        <span className="gold">Internships</span>
       </div>
-      <h1>Internships!</h1>
+
+      <div className="cod">Hello cod</div>
     </div>
   );
 };
