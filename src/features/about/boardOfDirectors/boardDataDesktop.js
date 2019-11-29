8b91af284e6a3364be637cab11601f4b1e0e15d6
github username <joanrigdon@gmail.com>
2019-11-29 14:47:38 -0500

    fixed mobile view

--- src/features/about/boardOfDirectors/boardDataDesktop.js
+++ src/features/about/boardOfDirectors/boardDataDesktop.js
@@ -5,7 +5,7 @@ export const boardDataDesktop = (
   <Grid columns={2}>
     <Grid.Row>
       <Grid.Column>
-        <div className="board-card">
+        <div className="board-card center">
           <h1 className="board-header">Debra Ray</h1>
           <div className="board-subhead">Founder and CEO</div>
           <br />
@@ -26,7 +26,7 @@ export const boardDataDesktop = (
         </div>
       </Grid.Column>
       <Grid.Column>
-        <div className="board-card">
+        <div className="board-card center">
           <img
             className="board-photo"
             src="https://media.licdn.com/dms/image/C4D03AQEX65R-YqCvDw/profile-displayphoto-shrink_200_200/0?e=1579737600&v=beta&t=ZLLmNnAa_u3iCWgjD-TNNuQX4K0uhNWZJoklxttN9c0"
@@ -38,7 +38,7 @@ export const boardDataDesktop = (
 
     <Grid.Row>
       <Grid.Column>
-        <div className="board-card">
+        <div className="board-card center">
           <img
             className="board-photo"
             src="https://static.wixstatic.com/media/47ab14_85e2c9c81a0a45b6b2b6ee0623d1f9f3~mv2.jpg/v1/fill/w_420,h_442,al_c,q_80,usm_0.66_1.00_0.01/Image%20from%20iOS_edited.webp"
@@ -48,7 +48,7 @@ export const boardDataDesktop = (
       </Grid.Column>
 
       <Grid.Column>
-        <div className="board-card">
+        <div className="board-card center">
           <h1 className="board-header">Tom Sullivan</h1>
           <div className="board-subhead">President</div>
           <br />
@@ -71,73 +71,3 @@ export const boardDataDesktop = (
     </Grid.Row>
   </Grid>
 );
-
-{
-  /* <>
-    <div className="board-row">
-      <div className="board-column">
-        <div className="board-card">
-          <h1 className="board-header">Debra Ray</h1>
-          <div className="board-subhead">Founder and CEO</div>
-          <br />
-          <div class="description board-description">
-            "I want to advance global economic development by sharing resources
-            with women entering adulthood to help them become more independent."
-          </div>
-          <br />
-          <Popup
-            content="Find me on LinkedIn"
-            trigger={
-              <a href="https://www.linkedin.com/in/debraannray/">
-                <Icon name="linkedin" size="huge" className="blue linked-in" />
-              </a>
-            }
-            position="bottom center"
-          />
-        </div>
-      </div>
-      <div className="board-column">
-        <div className="board-card">
-          <img
-            className="board-photo"
-            src="https://media.licdn.com/dms/image/C4D03AQEX65R-YqCvDw/profile-displayphoto-shrink_200_200/0?e=1579737600&v=beta&t=ZLLmNnAa_u3iCWgjD-TNNuQX4K0uhNWZJoklxttN9c0"
-            alt="headshot of board member Debra Ray"
-          />
-        </div>
-      </div>
-    </div>
-
-    <div className="baord-row">
-      <div className="board-column">
-        <div className="board-card">
-          <img
-            className="board-photo"
-            src="https://static.wixstatic.com/media/47ab14_85e2c9c81a0a45b6b2b6ee0623d1f9f3~mv2.jpg/v1/fill/w_420,h_442,al_c,q_80,usm_0.66_1.00_0.01/Image%20from%20iOS_edited.webp"
-            alt="headshot of board member Tom Sullivan"
-          />
-        </div>
-      </div>
-      <div className="board-column">
-        <div className="board-card">
-          <h1 className="board-header">Tom Sullivan</h1>
-          <div className="board-subhead">President</div>
-          <br />
-          <div class="description board-description">
-            "I want to help the youth of tomorrow turn their creative ideas into
-            reality with computer programming."
-          </div>
-          <br />
-          <Popup
-            content="Find me on LinkedIn"
-            trigger={
-              <a href="https://www.linkedin.com/in/tom-sullivan-a925421a/">
-                <Icon name="linkedin" size="huge" className="blue linked-in" />
-              </a>
-            }
-            position="bottom center"
-          />
-        </div>
-      </div>
-    </div>
-  </> */
-}
