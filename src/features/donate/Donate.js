254ccfe18f57902155d1da063a7f58b6b6bfce16
github username <joanrigdon@gmail.com>
2019-11-29 19:18:50 -0500

    added link

--- src/features/donate/Donate.js
+++ src/features/donate/Donate.js
@@ -1,37 +1,40 @@
-import React from "react"
-import "./Donate.css"
-import { Container, Card } from "semantic-ui-react"
-import { donateData } from "./DonateData"
-import DonateCard from "./DonateCard"
-import Contact from "./contact/Contact"
+import React from "react";
+import "./Donate.css";
+import { Container, Card } from "semantic-ui-react";
+import { donateData } from "./DonateData";
+import DonateCard from "./DonateCard";
+import Contact from "./contact/Contact";
 
 const Donate = () => {
-	return (
-		<>
-			<Container className="main-container center">
-				<div className="headline banner-headline">
-					<span className="gold">Donate</span>
-				</div>
+  return (
+    <>
+      <Container className="main-container center">
+        <div className="headline banner-headline">
+          <span className="gold">Donate</span>
+        </div>
 
-				<div className="subhead">
-					You can promote technology careers for women in Ecuador
-					<br /> by helping us with supplies, equipment, and space
-					from which to work.
-				</div>
-				<br />
-				<br />
+        <div className="subhead">
+          You can promote technology careers for women in Ecuador
+          <br /> by helping us with supplies, equipment, and space from which to
+          work.
+        </div>
+        <br />
+        <br />
 
-				<Card.Group itemsPerRow={3} className="center">
-					{donateData.map((donate) => (
-						<DonateCard {...donate} padding="20px" />
-					))}
-				</Card.Group>
+        <Card.Group itemsPerRow={3} className="center">
+          {donateData.map(donate => (
+            <DonateCard {...donate} padding="20px" />
+          ))}
+        </Card.Group>
 
-				<Contact className="contact" />
-			
-			</Container>
-		</>
-	)
-}
+        <a href="https://charity.gofundme.com/o/en/campaign/promote-women-in-tech-careers-in-ecuador/codigoecuador">
+          <h1>See our GoFundMe page</h1>
+        </a>
 
-export default Donate
+        <Contact className="contact" />
+      </Container>
+    </>
+  );
+};
+
+export default Donate;
