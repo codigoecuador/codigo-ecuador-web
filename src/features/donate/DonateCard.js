3cc566b78eefa7d6479c5e43b6013a0fcc7c1008
github username <joanrigdon@gmail.com>
2019-11-30 03:44:19 -0500

    fixed donate cards - no more blue border at bottom

--- src/features/donate/DonateCard.js
+++ src/features/donate/DonateCard.js
@@ -3,26 +3,21 @@ import { Card } from "semantic-ui-react";
 
 const DonateCard = ({ topic, summary, image }) => {
   return (
-    <div className='donate-cards-group'>
-      <Card>
-        <div className='ui massive fade reveal image'>
+    <div className="donate-cards-group">
+      <Card className="donate-card">
+        <div className="ui massive fade reveal image">
           <img
-            className='visible content donate-card'
+            className="visible content donate-card"
             src={image}
-            alt='laptop, building or meeting space'
+            alt="laptop, building or meeting space"
           />
-          <div className='hidden-content'>
-            <h1 className='gold'>{topic}</h1>
+          <div className="hidden-content">
+            <h1 className="gold">{topic}</h1>
             <h3>{summary}</h3>
             <br />
           </div>
         </div>
       </Card>
-
-      <br />
-      <br />
-      <br />
-      <br />
     </div>
   );
 };
