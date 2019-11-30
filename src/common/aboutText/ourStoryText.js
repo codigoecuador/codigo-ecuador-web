e36d6c35f3a15af9ae72cff2e30fa53ac8016e53
github username <joanrigdon@gmail.com>
2019-11-30 02:14:14 -0500

    added spanish text to our story text object

--- src/common/aboutText/ourStoryText.js
+++ src/common/aboutText/ourStoryText.js
@@ -1,59 +1,54 @@
-import React from "react"
+import React from "react";
 
 export const ourStoryText = {
-	EN: {
-		goldTitle: "Our",
-		navyTitle: " Story",
-		text: (
-			<div>
-				<p>
-					At Código Ecuador, we care about creating a more equal world
-					<br /> between countries and across genders.
-					<br />
-					We want to train young women to take on technology careers
-					in Ecuador.
-				</p>
+  EN: {
+    goldTitle: "Our",
+    navyTitle: " Story",
+    text: (
+      <div>
+        <p>
+          At Código Ecuador, we care about creating a more equal world
+          <br /> between countries and across genders.
+          <br />
+          We want to train young women to take on technology careers in Ecuador.
+        </p>
 
-				<p>
-					Our world has a shortage of technology talent and our global
-					economy is plagued by inequality. Research shows that
-					advancing technological skills in developing countries opens
-					up many opportunities that would not otherwise be possible.{" "}
-					<a href="/resources/research">
-						<strong>See the research.</strong>
-					</a>
-				</p>
-				<br />
-			</div>
-		),
-	},
+        <p>
+          Our world has a shortage of technology talent and our global economy
+          is plagued by inequality. Research shows that advancing technological
+          skills in developing countries opens up many opportunities that would
+          not otherwise be possible.{" "}
+          <a href="/resources/research">
+            <strong>See the research.</strong>
+          </a>
+        </p>
+        <br />
+      </div>
+    )
+  },
 
-	ES: {
-		goldTitle: "Nuestra",
-		navyTitle: " Historia",
-		text: (
-			<div>
-				<p>
-					(spanish)At Código Ecuador, we care about creating a more
-					equal world
-					<br /> between countries and across genders.
-					<br />
-					(spanish)We want to train young women to take on technology
-					careers in Ecuador.
-				</p>
+  ES: {
+    goldTitle: "Nuestra",
+    navyTitle: " Historia",
+    text: (
+      <div>
+        <p>
+          En Código Ecuador, nos preocupamos por crear un mundo más igualitario,
+          entre países y entre géneros. Queremos capacitar a mujeres jóvenes
+          para que emprendan carreras tecnológicas en Ecuador. ​
+        </p>
 
-				<p>
-					(spanish)Our world has a shortage of technology talent and
-					our global economy is plagued by inequality. Research shows
-					that advancing technological skills in developing countries
-					opens up many opportunities that would not otherwise be
-					possible.{" "}
-					<a href="/resources/research">
-						<strong>See the research in spanish.</strong>
-					</a>
-				</p>
-				<br />
-			</div>
-		),
-	}
-}
+        <p>
+          Hay una escasez mundial de talento tecnológico, y la economía global
+          está plagada de desigualdades. El avance de las habilidades
+          tecnológicas en los países en desarrollo abrirá muchas oportunidades
+          que de otro modo no serían posibles.
+          <a href="/resources/research">
+            <strong>Las Investigaciones</strong>
+          </a>
+        </p>
+        <br />
+      </div>
+    )
+  }
+};
