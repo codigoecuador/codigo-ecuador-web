b920a82e2269612bac16982d73a82ab451687976
github username <joanrigdon@gmail.com>
2019-11-30 01:40:59 -0500

    restored background image to internships

--- src/features/services/internships/Internships.js
+++ src/features/services/internships/Internships.js
@@ -4,23 +4,23 @@ import "./Internships.css";
 import { internshipText } from "../../../common/servicesText/internshipText.js";
 
 const Internships = props => {
-  
   let lang = localStorage.getItem("language");
-  console.log()
+  console.log();
   return (
     <div className="main-container">
       <div className="headline banner-headline">
         <span className="gold">{internshipText[lang].title}</span>
       </div>
-      <h1>{internshipText[lang].text}</h1>
 
+      <div className="internships-container">
+        <div className="internships-text">{internshipText[lang].text}</div>
+      </div>
     </div>
   );
 };
 
 const mapStateToProps = state => {
-  
-  return { language: state.language};
+  return { language: state.language };
 };
 
 export default connect(mapStateToProps)(Internships);
