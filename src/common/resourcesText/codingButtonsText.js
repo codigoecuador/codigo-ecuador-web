53d9cb1bd9b2ceddc6a48d8182ac1e0bf2cc1a32
bebopvinh <vinhdo07@gmail.com>
2019-11-29 18:45:13 -0800

    Add Spanish translations

---
+++ src/common/resourcesText/codingButtonsText.js
@@ -0,0 +1,27 @@
+export const codingButtonsText = {
+  EN: [
+    { name: "Student", text: "For Students" },
+    { name: "Parent", text: "For Parents" },
+    { name: "Educator", text: "For Educators" },
+    { name: "Entrepreneur", text: "For Entrepreneurs" },
+  ],
+
+  ES: [
+    {
+      name: "Student",
+      text: "Para Estudiantes",
+    },
+    {
+      name: "Parent",
+      text: "Para Padres",
+    },
+    {
+      name: "Educator",
+      text: "Para Educadores",
+    },
+    {
+      name: "Entrepreneur",
+      text: "Para Emprendedores",
+    },
+  ],
+}
