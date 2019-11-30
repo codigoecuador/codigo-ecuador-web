83b96ed3eba083cd9b34937d1e0f00ebc01e3d7c
github username <joanrigdon@gmail.com>
2019-11-30 14:28:16 -0500

    set up photo credit for home page photo, still looking for non-invasive way to display

---
+++ src/common/homeText/homeText.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const homeText = {
+  EN: {
+    photoCredit: "photo by Omar Lopez on Unsplash.com"
+  },
+
+  ES: {
+    photoCredit: "foto por Omar Lopez on Unsplash.com"
+  }
+};
--- src/features/home/CoverPic.js
+++ src/features/home/CoverPic.js
@@ -1,16 +1,18 @@
-import React from "react"
-import womenCodigo from "./womenCodigo.jpg"
+import React from "react";
+import { Popup } from "semantic-ui-react";
+import womenCodigo from "./womenCodigo.jpg";
+import { homeText } from "../../common/homeText/homeText";
 
 const CoverPic = () => {
-	return (
-		<div>
-			<img
-				src={womenCodigo}
-				alt="young women in a field, smiling as they face the camera"
-				className="cover-pic"
-			/>
-		</div>
-	)
-}
+  return (
+    <div>
+      <img
+        src={womenCodigo}
+        alt="young women in a field, smiling as they face the camera. Photo by @OmarLopez1 on Unsplash.com"
+        className="cover-pic"
+      />
+    </div>
+  );
+};
 
-export default CoverPic
+export default CoverPic;
