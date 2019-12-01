3a6b7b07af9dfcc411e184a4afc2345952bb7a9a
github username <joanrigdon@gmail.com>
2019-12-01 18:49:22 -0500

    added link to medium on blog posts page

--- src/common/blogText/blogText.js
+++ src/common/blogText/blogText.js
@@ -1,11 +1,14 @@
 export const blogText = {
   EN: {
     title: "Recent Blog Posts",
+    subtitle: "Click here to see all our blog posts on Medium.",
     error: "No blog posts at the moment!",
     by: "by"
   },
   ES: {
     title: "Entradas recientes del blog",
+    subtitle:
+      "Haga clic aquí para ver todas nuestras publicaciones de blog en Medium.",
     error: "¡No hay entradas en el blog al momento!",
     by: "por"
   }
