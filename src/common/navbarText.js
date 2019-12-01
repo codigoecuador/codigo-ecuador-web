bd699d58b78088bb7d6dc1904578965a1802989d
github username <joanrigdon@gmail.com>
2019-12-01 14:18:48 -0500

    added navbar text object

---
+++ src/common/navbarText.js
@@ -0,0 +1,36 @@
+export const navbarText = {
+  EN: {
+    home: "Home",
+    about: "About",
+    ourStory: "Our Story",
+    partners: "Our Partners",
+    team: "Meet the Team",
+    board: "Board of Directors",
+    services: "Services",
+    classes: "Computer Programming Courses",
+    internships: "Experiential Learning",
+    mentorship: "Career Mentorship",
+    resources: "Resources",
+    coding: "Coding",
+    research: "Research",
+    blog: "Blog",
+    donate: "Donate"
+  },
+  ES: {
+    home: "Home",
+    about: "Sobre",
+    ourStory: "Nuestra Historia",
+    partners: "Nuestros Socios",
+    team: "Conozca al Equipo",
+    board: "Junta Directiva",
+    services: "Servicios",
+    classes: "Cursos de Programación",
+    internships: "Aprendizaje Experiencial",
+    mentorship: "Mentoría de Carrera",
+    resources: "Recursos",
+    coding: "Recursos de Codificación",
+    research: "Investigaciones",
+    blog: "Blog",
+    donate: "Donar"
+  }
+};
