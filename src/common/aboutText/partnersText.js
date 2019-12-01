82512fe73e413e0be696b94e13580e5e92cf99aa
github username <joanrigdon@gmail.com>
2019-12-01 10:18:54 -0500

    connected minorPartnersData to new PartnersText object

---
+++ src/common/aboutText/partnersText.js
@@ -0,0 +1,133 @@
+import React from "react";
+import kodluyoruzLogo from "./kodluyoruz.webp";
+import notSlackersLogo from "./notSlackersLogo.png";
+
+export const partnersText = {
+  EN: {
+    goldTitle: "Our",
+    navyTitle: " Partners",
+    subtitle: (
+      <p>
+        We are grateful to our partners who share expertise and resources to
+        help us succeed.
+      </p>
+    ),
+    juventudTitle: "Juventud del Mañana",
+    juventudText: (
+      <>
+        <p>
+          Juventud del Mañana is an association of dedicated, community - minded
+          people who saw and responded to the needs of our youth.
+        </p>
+        <p>
+          It is a teen drop-in centre that provides a safe,
+          drug-and-alcohol-free, place designed exclusively to meet their needs.
+          It is a place to enjoy their activities, whether it be playing
+          basketball, practicing with their band, playing games, listening to
+          music, and holding dances.
+        </p>
+        <p>
+          Beginning in October, Juventud del Mañana will provide educational
+          programs and on the job experiences designed to help teens reach their
+          full potential.
+        </p>
+      </>
+    ),
+
+    //minor partners
+
+    kodName: "Juventud del Mañana",
+    kodImage: kodluyoruzLogo,
+    kodText: (
+      <p>
+        Kodluyoruz.org is a software movement that promotes code literacy and
+        adds value to the future of young people in the software field in order
+        to improve every segment of society by using technology."
+      </p>
+    ),
+
+    notSlackersName: "!Slackers",
+    notSlackersText: (
+      <>
+        <p>
+          The !Slackers (aka 'The Not Slackers') learned to code together in the
+          same cohort at{" "}
+          <a href="https://flatironschool.com/career-courses/coding-bootcamp/online/north-america">
+            The Flatiron School.
+          </a>{" "}
+          After graduating in summer 2019, seven members of the cohort built our
+          website from the ground up. If you need full-stack developers, hire
+          them!
+          <a href="https://github.com/joanrig/codigo-ecuador-web/graphs/contributors">
+            {" "}
+            You can see their Github profiles here.
+          </a>
+        </p>
+      </>
+    ),
+
+    becomeAPartnerName: "Become a Partner",
+    becomeAPartnerText: (
+      <p>
+        If you are looking for talent or want to have a partner available to
+        share best practices and resources, please feel free to reach out to us
+        with your information. We are always looking to form new connections
+        with businesses and organizations that share our values.
+      </p>
+    ),
+
+    placeholder: "Placeholder",
+    placeholderText: (
+      <p>
+        Here is some information about a potential partner. Here is some
+        information about a potential partner. Here is some information about a
+        potential partner. Here is some information about a potential partner.
+      </p>
+    )
+  },
+
+  ES: {
+    goldTitle: "Our",
+    navyTitle: " Partners",
+    subtitle: (
+      <p>
+        We are grateful to our partners who share expertise and resources to
+        help us succeed.
+      </p>
+    ),
+    juventudTitle: "Juventud del Mañana",
+    juventudText: (
+      <>
+        <p>
+          La Juventud del Mañana es una asociación de personas dedicadas y
+          comunitarias que vieron y respondieron a las necesidades de nuestros
+          jóvenes.
+        </p>
+        <p>
+          Es un centro de entrega para adolescentes que proporciona un lugar
+          seguro, libre de drogas y sin alcohol, diseñado exclusivamente para
+          satisfacer sus necesidades. Es un lugar para disfrutar de sus
+          actividades, ya sea jugando al baloncesto, practicando con su banda,
+          jugando, escuchando música y bailando.
+        </p>
+        <p>
+          A partir de Octubre, Juventud del Mañana ofrecerá programas
+          educativos.
+        </p>
+      </>
+    ),
+
+    //minor partners
+    kodTitle: "Juventud del Mañana",
+    kodText: <p>kod text</p>,
+
+    notSlackersName: "!Slackers",
+    notSlackersText: <p>not slackers text</p>,
+
+    becomeAPartnerName: "Become a Partner",
+    becomeAPartnerText: <p>become a partner text</p>,
+
+    placeholderName: "Placeholder",
+    placeholderText: <p>placeholder text</p>
+  }
+};
--- src/features/about/partners/minorPartners/MinorPartnersData.js
+++ src/features/about/partners/minorPartners/MinorPartnersData.js
@@ -1,18 +1,13 @@
+import React from "react";
 import kodluyoruzLogo from "./kodluyoruz.webp";
 import notSlackersLogo from "./notSlackersLogo.png";
-import React from "react";
+import { partnersText } from "../../../../common/aboutText/partnersText";
 
 const kod = {
   name: "Kodluyoruz",
   image: kodluyoruzLogo,
   imageClassName: "kod-logo",
-  content: [
-    <p>
-      Kodluyoruz.org is a software movement that promotes code literacy and adds
-      value to the future of young people in the software field in order to
-      improve every segment of society by using technology."
-    </p>
-  ],
+  text: <>{partnersText[localStorage.getItem("language")].kodText}</>,
   link: "https://www.kodluyoruz.org/"
 };
 
@@ -20,37 +15,20 @@ const notSlackers = {
   name: "!Slackers",
   image: notSlackersLogo,
   imageClassName: "not-slackers-logo",
-  content: [
-    <p>
-      The !Slackers (aka 'The Not Slackers') learned to code together in the
-      same cohort at{" "}
-      <a href="https://flatironschool.com/career-courses/coding-bootcamp/online/north-america">
-        The Flatiron School.
-      </a>{" "}
-      After graduating in summer 2019, seven members of the cohort built our
-      website from the ground up. If you need full-stack developers, hire them!
-      <a href="https://github.com/joanrig/codigo-ecuador-web/graphs/contributors">
-        {" "}
-        You can see their Github profiles here.
-      </a>
-    </p>
-  ],
+  text: <>{partnersText[localStorage.getItem("language")].notSlackersText}</>,
   link: "https://www.google.com/"
 };
 
 const becomeAPartner = {
-  name: "Become a Partner",
+  name: (
+    <>{partnersText[localStorage.getItem("language")].becomeAPartnerName}</>
+  ),
   image:
     "https://static.wixstatic.com/media/nsplsh_54616d4d6272346f6b7634~mv2_d_6016_4016_s_4_2.jpg/v1/fill/w_246,h_164,al_c,q_80,usm_0.66_1.00_0.01,blur_2/nsplsh_54616d4d6272346f6b7634~mv2_d_6016_4016_s_4_2.jpg",
   imageClassName: "become-partner-logo",
-  content: [
-    <p>
-      If you are looking for talent or want to have a partner available to share
-      best practices and resources, please feel free to reach out to us with
-      your information. We are always looking to form new connections with
-      businesses and organizations that share our values.
-    </p>
-  ],
+  content: (
+    <>{partnersText[localStorage.getItem("language")].becomeAPartnerText}</>
+  ),
   link: "mailto:info@codigoecuador.com"
 };
 
@@ -59,13 +37,9 @@ const placeholder = {
   image:
     "https://images.unsplash.com/photo-1573641287741-f6e223d81a0f?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
   imageClassName: "placeholder4-logo",
-  content: [
-    <p>
-      Here is some information about a potential partner. Here is some
-      information about a potential partner. Here is some information about a
-      potential partner. Here is some information about a potential partner.
-    </p>
-  ],
+  content: (
+    <>{partnersText[localStorage.getItem("language")].placeholderText}</>
+  ),
   link: "https://www.google.com/"
 };
 
